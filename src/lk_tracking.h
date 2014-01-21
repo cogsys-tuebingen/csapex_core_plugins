09f2cc648ae278a3f356902f543f2c845b54ce87
buck <sebastian.buck@uni-tuebingen.de>
2014-01-21 16:12:36 +0100

    added generic lk tracking (incomplete)

---
+++ src/lk_tracking.h
@@ -0,0 +1,43 @@
+#ifndef FILTER_TOOL_DETECTION_H
+#define FILTER_TOOL_DETECTION_H
+
+/// COMPONENT
+#include <csapex/model/node.h>
+
+/// SYSTEM
+#include <opencv2/opencv.hpp>
+
+namespace csapex
+{
+
+class LKTracking : public csapex::Node
+{
+public:
+    LKTracking();
+
+public:
+    virtual void allConnectorsArrived();
+    virtual void setup();
+
+    void reset();
+
+private:
+    void update(const param::Parameter *);
+
+private:
+    ConnectorIn* in_image_;
+    ConnectorIn* in_keypoints_;
+
+    ConnectorOut* out_debug_;
+
+    bool init_;
+
+    std::vector<cv::Point2f> points[2];
+
+    cv::Mat gray;
+    cv::Mat prevGray;
+};
+
+}
+
+#endif // FILTER_TOOL_DETECTION_H
