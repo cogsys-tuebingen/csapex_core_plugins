206ae2fd1cb4085082f7dba239c1cd65e828cce0
buck <sebastian.buck@uni-tuebingen.de>
2014-04-22 01:08:44 +0200

    ported color converter

--- src/filter_colorconvert.cpp
+++ src/filter_colorconvert.cpp
@@ -4,12 +4,11 @@
 /// PROJECT
 #include <csapex/model/connector_in.h>
 #include <csapex/model/connector_out.h>
-#include <csapex/view/box.h>
+#include <utils_param/parameter_factory.h>
 
 /// SYSTEM
 #include <csapex/utility/register_apex_plugin.h>
-#include <QComboBox>
-#include <QLabel>
+#include <boost/assign/std.hpp>
 
 CSAPEX_REGISTER_CLASS(vision_plugins::ColorConvert, csapex::Node)
 
@@ -18,8 +17,18 @@ using namespace csapex;
 using namespace csapex::connection_types;
 
 ColorConvert::ColorConvert()
-    : combo_in_(NULL)
 {
+    std::map<std::string, int> encodings = boost::assign::map_list_of
+            ("YUV", (int) YUV)
+            ("RGB", (int) RGB)
+            ("BGR", (int) BGR)
+            ("HSL", (int) HSL)
+            ("HSV", (int) HSV)
+            ("MONO", (int) MONO);
+
+    addParameter(param::ParameterFactory::declareParameterSet("input", encodings));
+    addParameter(param::ParameterFactory::declareParameterSet("output", encodings));
+
     cs_pair_to_operation_.insert(csiPair(csPair(RGB, BGR), (int) CV_RGB2BGR));
     cs_pair_to_operation_.insert(csiPair(csPair(BGR, RGB), (int) CV_BGR2RGB));
 
@@ -55,35 +64,21 @@ ColorConvert::~ColorConvert()
 {
 }
 
-void ColorConvert::fill(QBoxLayout *parent)
+void ColorConvert::setup()
 {
     setSynchronizedInputs(true);
 
     input_img_ = addInput<CvMatMessage>("Image");
     output_img_ = addOutput<CvMatMessage>("Image");
-
-    combo_in_ = new QComboBox;
-    combo_out_ = new QComboBox;
-    fillCombo(combo_in_, index_to_cs_in_);
-    fillCombo(combo_out_, index_to_cs_out_);
-
-    parent->addWidget(new QLabel("Input CS"));
-    parent->addWidget(combo_in_);
-    parent->addWidget(new QLabel("Output CS"));
-    parent->addWidget(combo_out_);
 }
 
 void ColorConvert::process()
 {
     CvMatMessage::Ptr img = input_img_->getMessage<CvMatMessage>();
 
-    /// MEMENTO
-    state_.input_index = combo_in_->currentIndex();
-    state_.output_index = combo_out_->currentIndex();
-
     csPair cspair;
-    cspair.first  = index_to_cs_in_[state_.input_index];
-    cspair.second = index_to_cs_out_[state_.output_index];
+    cspair.first  = static_cast<ColorSpace> (param<int>("input"));
+    cspair.second = static_cast<ColorSpace> (param<int>("output"));
 
     CvMatMessage::Ptr out(new CvMatMessage(cs_to_encoding_[cspair.second]));
 
@@ -108,45 +103,5 @@ void ColorConvert::process()
         out = img;
     }
 
-
     output_img_->publish(out);
-}
-
-Memento::Ptr ColorConvert::getState() const
-{
-    return boost::shared_ptr<State>(new State(state_));
-}
-
-void ColorConvert::setState(Memento::Ptr memento)
-{
-    boost::shared_ptr<State> m = boost::dynamic_pointer_cast<State> (memento);
-    assert(m.get());
-
-    state_ = *m;
-
-    if(combo_in_) {
-        combo_in_->setCurrentIndex(state_.input_index);
-        combo_out_->setCurrentIndex(state_.output_index);
-    }
-}
-
-bool ColorConvert::usesMask()
-{
-    return false;
-}
-
-void ColorConvert::fillCombo(QComboBox *combo, std::map<int, ColorSpace> &map)
-{
-    combo->addItem("BGR");
-    map.insert(icsPair(combo->findText("BGR"), BGR));
-    combo->addItem("RGB");
-    map.insert(icsPair(combo->findText("RGB"), RGB));
-    combo->addItem("HSV");
-    map.insert(icsPair(combo->findText("HSV"), HSV));
-    combo->addItem("HSL");
-    map.insert(icsPair(combo->findText("HSL"), HSL));
-    combo->addItem("YUV");
-    map.insert(icsPair(combo->findText("YUV"), YUV));
-    combo->addItem("MONO");
-    map.insert(icsPair(combo->findText("MONO"), MONO));
 }
--- src/filter_colorconvert.h
+++ src/filter_colorconvert.h
@@ -5,21 +5,17 @@
 #include <csapex/csapex_fwd.h>
 
 /// COMPONENT
-#include <csapex/model/boxed_object.h>
+#include <csapex/model/node.h>
 #include <csapex_vision/cv_mat_message.h>
 
-
-class QComboBox;
 
 namespace vision_plugins {
 /**
  * @brief The ColorConvert class can be used to convert colored images from one color space
  *        to another.
  */
-class ColorConvert : public csapex::BoxedObject
+class ColorConvert : public csapex::Node
 {
-    Q_OBJECT
-
 public:
     /**
      * @brief ColorConvert default constructor.
@@ -32,15 +28,11 @@ public:
     /**
      * @brief See base class definition.
      */
-    virtual void fill(QBoxLayout *parent);
+    virtual void setup();
     /**
      * @brief See base class definition.
      */
     virtual void process();
-
-    /// MEMENTO
-    void                 setState(csapex::Memento::Ptr memento);
-    csapex::Memento::Ptr getState() const;
 
 protected:
     /// internal typdefs
@@ -54,36 +46,8 @@ protected:
     std::map<int, ColorSpace> index_to_cs_in_;
     std::map<int, ColorSpace> index_to_cs_out_;
 
-    QComboBox *combo_in_;
-    QComboBox *combo_out_;
-
     csapex::ConnectorIn* input_img_;
     csapex::ConnectorOut* output_img_;
-
-    virtual bool usesMask();
-    void fillCombo(QComboBox *combo, std::map<int, ColorSpace> &map);
-
-    /// MEMENTO
-    class State : public csapex::Memento {
-    public:
-        void readYaml(const YAML::Node &node)
-        {
-            node["input_index"] >> input_index;
-            node["output_index"] >> output_index;
-        }
-
-        void writeYaml(YAML::Emitter &out) const
-        {
-            out << YAML::Key << "input_index" << YAML::Value << input_index;
-            out << YAML::Key << "output_index" << YAML::Value << output_index;
-        }
-
-    public:
-        int     input_index;
-        int     output_index;
-    };
-
-    State state_;
 };
 
 }
