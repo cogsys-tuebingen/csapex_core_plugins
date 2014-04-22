25c6903ecaf19f88ce738befe6cabef65337fc31
Richard Hanten <richard.hanten@uni-tuebingen.de>
2014-04-22 20:29:09 +0200

    restruct

---
+++ src/combine/absolute_difference.cpp
@@ -0,0 +1,5 @@
+#include "absolute_difference.h"
+
+AbsoluteDifference::AbsoluteDifference()
+{
+}
---
+++ src/combine/absolute_difference.h
@@ -0,0 +1,10 @@
+#ifndef ABSOLUTE_DIFFERENCE_H
+#define ABSOLUTE_DIFFERENCE_H
+
+class AbsoluteDifference
+{
+public:
+    AbsoluteDifference();
+};
+
+#endif // ABSOLUTE_DIFFERENCE_H
