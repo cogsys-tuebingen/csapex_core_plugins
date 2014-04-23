0dcbfabdf39e464ac1dfdf00f97e1d22a48380a2
Richard Hanten <richard.hanten@uni-tuebingen.de>
2014-04-24 00:05:47 +0200

    noise filter threshed

--- src/experimental/threshold_noise_filter.cpp
+++ src/experimental/threshold_noise_filter.cpp
@@ -18,16 +18,58 @@ CSAPEX_REGISTER_CLASS(vision_plugins::ThresholdNoiseFilter, csapex::Node)
 
 ThresholdNoiseFilter::ThresholdNoiseFilter()
 {
+    addTag(Tag::get("Experimental"));
+    addTag(Tag::get("Vision"));
+    addTag(Tag::get("vision_plugins"));
+
+    addParameter(param::ParameterFactory::declareRange("threshold", 0, 255, 255, 1));
 }
 
 
 void ThresholdNoiseFilter::process()
 {
-    utils_cv::ThresholdNoiseFilter<float> T;
+    CvMatMessage::Ptr input     = input_->getMessage<CvMatMessage>();
+    CvMatMessage::Ptr threshold = threshold_->getMessage<CvMatMessage>();
+    CvMatMessage::Ptr output(new CvMatMessage(input->getEncoding()));
+
+    if(threshold->getEncoding() != enc::mono) {
+        throw std::runtime_error("Threshold needs to be mono!");
+    }
+
+
+    uchar thresh = param<int>("threshold");
+    int   type   = input->value.type();
+    switch(type) {
+    case CV_8UC1:
+        utils_cv::ThresholdNoiseFilter<uchar,uchar>::apply(input->value, threshold->value, thresh, output->value);
+        break;
+    case CV_8SC1:
+        utils_cv::ThresholdNoiseFilter<char,uchar>::apply(input->value, threshold->value, thresh, output->value);
+        break;
+    case CV_16UC1:
+        utils_cv::ThresholdNoiseFilter<ushort,uchar>::apply(input->value, threshold->value, thresh, output->value);
+        break;
+    case CV_16SC1:
+        utils_cv::ThresholdNoiseFilter<short,uchar>::apply(input->value, threshold->value, thresh, output->value);
+        break;
+    case CV_32SC1:
+        utils_cv::ThresholdNoiseFilter<int,uchar>::apply(input->value, threshold->value, thresh, output->value);
+        break;
+    case CV_32FC1:
+        utils_cv::ThresholdNoiseFilter<float,uchar>::apply(input->value, threshold->value, thresh, output->value);
+        break;
+    case CV_64FC1:
+        utils_cv::ThresholdNoiseFilter<double,uchar>::apply(input->value, threshold->value, thresh, output->value);
+        break;
+    }
+
+    output_->publish(output);
 }
 
 void ThresholdNoiseFilter::setup()
 {
+    setSynchronizedInputs(true);
+
     input_      = addInput<CvMatMessage>("unfiltered");
     threshold_  = addInput<CvMatMessage>("weights");
     output_     = addOutput<CvMatMessage>("filtered");
