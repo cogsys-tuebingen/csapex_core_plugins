d1b2d694b3ea16cd92f69f4bc6a3289c0b752869
Richard Hanten <richard.hanten@uni-tuebingen.de>
2014-05-12 23:39:12 +0200

    minor

--- src/visualize/render_histogram.cpp
+++ src/visualize/render_histogram.cpp
@@ -35,7 +35,9 @@ RenderHistogram::RenderHistogram() :
 
 void RenderHistogram::process()
 {
+
 #warning "FIX ENCODING"
+
     boost::shared_ptr<std::vector<CvMatMessage::Ptr> const> in = input_->getMessage<GenericVectorMessage, CvMatMessage::Ptr>();
     CvMatMessage::Ptr out(new CvMatMessage(enc::bgr));
     out->value = cv::Mat(height_, width_, CV_8UC3, cv::Scalar(0,0,0));
