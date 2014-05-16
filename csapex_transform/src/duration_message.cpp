547979e5dc142bcf974b9d3c34f5feabd5dda35a
Robert Pech <robert.pech@uni-tuebingen.de>
2014-05-16 10:46:31 +0200

    duration message

---
+++ csapex_transform/src/duration_message.cpp
@@ -0,0 +1,24 @@
+/// HEADER
+#include <csapex_transform/duration_message.h>
+
+/// SYSTEM
+#include <boost/date_time.hpp>
+
+using namespace csapex;
+using namespace connection_types;
+
+DurationMessage::DurationMessage()
+    : MessageTemplate<ros::Duration, DurationMessage> ("Duration")
+{}
+
+void DurationMessage::writeYaml(YAML::Emitter& yaml) {
+    yaml << YAML::Key << "duration" << YAML::Value << value.toNSec() << YAML::EndSeq;;
+}
+
+void DurationMessage::readYaml(const YAML::Node& node) {
+    if(exists(node, "duration")) {
+        u_int64_t nano;
+        node["duration"] >> nano;
+        value = value.fromNSec(nano);
+    }
+}
