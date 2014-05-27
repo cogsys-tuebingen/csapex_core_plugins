79d446322ec25f73388a264dde848057cb5a3c37
Richard Hanten <richard.hanten@uni-tuebingen.de>
2014-05-27 20:07:38 +0200

    label images

---
+++ vision_plugins/src/visualize/image_text_label.h
@@ -0,0 +1,25 @@
+#ifndef IMAGE_TEXT_LABEL_H
+#define IMAGE_TEXT_LABEL_H
+
+/// PROJECT
+#include <csapex/model/node.h>
+
+namespace vision_plugins {
+class ImageTextLabel : public csapex::Node
+{
+public:
+    ImageTextLabel();
+
+    void setup();
+    void process();
+
+protected:
+    enum Position {TOP_LEFT, BOTTOM_LEFT,
+                   TOP_RIGHT, BOTTOM_RIGHT};
+
+    csapex::ConnectorIn  *input_;
+    csapex::ConnectorOut *output_;
+
+};
+}
+#endif // IMAGE_TEXT_LABEL_H
