e3a7ace1a5d9e683d1bdea68d189a3cec6886381
buck <sebastian.buck@uni-tuebingen.de>
2014-07-16 16:55:22 +0200

    tmp. build fix - header was still MOCd

--- csapex_vision/src/plugin/image_roi_adapter.h
+++ csapex_vision/src/plugin/image_roi_adapter.h
@@ -1,82 +1,82 @@
 #ifndef SCAN_LABELER_ADAPTER_H
 #define SCAN_LABELER_ADAPTER_H
 
-/// PROJECT
-#include <csapex/view/default_node_adapter.h>
+///// PROJECT
+//#include <csapex/view/default_node_adapter.h>
 
-/// COMPONENT
-#include "image_roi.h"
+///// COMPONENT
+//#include "image_roi.h"
 
-/// SYSTEM
-#include <QGraphicsView>
+///// SYSTEM
+//#include <QGraphicsView>
 
-namespace csapex {
+//namespace csapex {
 
-class ImageRoiAdapter : public QObject, public DefaultNodeAdapter
-{
-    Q_OBJECT
+//class ImageRoiAdapter : public QObject, public DefaultNodeAdapter
+//{
+//    Q_OBJECT
 
-public:
-    ImageRoiAdapter(ImageRoi *node, WidgetController *widget_ctrl);
+//public:
+//    ImageRoiAdapter(ImageRoi *node, WidgetController *widget_ctrl);
 
-    virtual Memento::Ptr getState() const;
-    virtual void setState(Memento::Ptr memento);
+//    virtual Memento::Ptr getState() const;
+//    virtual void setState(Memento::Ptr memento);
 
-    virtual void setupUi(QBoxLayout* layout);
+//    virtual void setupUi(QBoxLayout* layout);
 
-    void setLabel(int label);
-public Q_SLOTS:
-    void display(lib_laser_processing::Scan* img);
-    void submit();
-    void labelSelected();
+//    void setLabel(int label);
+//public Q_SLOTS:
+//    void display(lib_laser_processing::Scan* img);
+//    void submit();
+//    void labelSelected();
 
-Q_SIGNALS:
-    void displayRequest(lib_laser_processing::Scan* img);
-    void submitRequest();
+//Q_SIGNALS:
+//    void displayRequest(lib_laser_processing::Scan* img);
+//    void submitRequest();
 
-protected:
-    bool eventFilter(QObject* o, QEvent* e);
-    void labelSelected(int label);
+//protected:
+//    bool eventFilter(QObject* o, QEvent* e);
+//    void labelSelected(int label);
 
-    ImageRoi* wrapped_;
+//    ImageRoi* wrapped_;
 
-    struct State : public Memento {
-        int width;
-        int height;
+//    struct State : public Memento {
+//        int width;
+//        int height;
 
-        State()
-            : width(300), height(300)
-        {}
+//        State()
+//            : width(300), height(300)
+//        {}
 
-        virtual void writeYaml(YAML::Emitter& out) const {
-            out << YAML::Key << "width" << YAML::Value << width;
-            out << YAML::Key << "height" << YAML::Value << height;
-        }
-        virtual void readYaml(const YAML::Node& node) {
-            node["width"] >> width;
-            node["height"] >> height;
-        }
-    };
+//        virtual void writeYaml(YAML::Emitter& out) const {
+//            out << YAML::Key << "width" << YAML::Value << width;
+//            out << YAML::Key << "height" << YAML::Value << height;
+//        }
+//        virtual void readYaml(const YAML::Node& node) {
+//            node["width"] >> width;
+//            node["height"] >> height;
+//        }
+//    };
 
 
-private:
-    connection_types::LabeledScanMessage::Ptr result_;
+//private:
+//    connection_types::LabeledScanMessage::Ptr result_;
 
-    QSize last_size_;
-    State state;
+//    QSize last_size_;
+//    State state;
 
-    QGraphicsPixmapItem* pixmap_;
+//    QGraphicsPixmapItem* pixmap_;
 
-    QGraphicsView* view_;
+//    QGraphicsView* view_;
 
-    QImage empty;
-    QPainter painter;
+//    QImage empty;
+//    QPainter painter;
 
-    int label_;
-    bool down_;
-    QPoint last_pos_;
-};
+//    int label_;
+//    bool down_;
+//    QPoint last_pos_;
+//};
 
-}
+//}
 
 #endif // SCAN_LABELER_ADAPTER_H
