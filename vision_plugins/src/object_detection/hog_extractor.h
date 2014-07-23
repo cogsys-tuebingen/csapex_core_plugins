e0eaa479958e0d25deb7373dd85b168b842897e8
Richard Hanten <richard.hanten@uni-tuebingen.de>
2014-07-23 15:49:03 +0200

    rois for training strategy

--- vision_plugins/src/object_detection/hog_extractor.h
+++ vision_plugins/src/object_detection/hog_extractor.h
@@ -16,8 +16,8 @@ public:
     void process();
 
 private:
-    csapex::ConnectorIn        *in_;
-    csapex::ConnectorOut       *out_;
+    csapex::ConnectorIn             *in_;
+    csapex::ConnectorOut            *out_;
 
     param::RangeParameter::Ptr       overlap_;
     void updateOverlap();
---
+++ vision_plugins/src/object_detection/hog_training_rois.cpp
@@ -0,0 +1,45 @@
+/// HEADER
+#include "hog_training_rois.h"
+
+/// PROJECT
+#include <csapex/model/connector_in.h>
+#include <csapex/model/connector_out.h>
+#include <csapex/utility/register_apex_plugin.h>
+#include <utils_param/parameter_factory.h>
+#include <csapex/model/node_modifier.h>
+
+/// SYSTEM
+//#include <boost/assign.hpp>
+
+CSAPEX_REGISTER_CLASS(csapex::HOGTrainingRois, csapex::Node)
+
+using namespace csapex;
+using namespace csapex::connection_types;
+
+
+HOGTrainingRois::HOGTrainingRois()
+{
+}
+
+void HOGTrainingRois::setupParameters()
+{
+    // addParameter(param::ParameterFactory::declareRange("range", 0.01, 1.0, 0.2, 0.01), update);
+
+    //std::map<std::string, int> featuremix = boost::assign::map_list_of
+    //        ("standard", 0)
+    //        ("no foo", 1)
+    //        ("no bar", 2);
+
+    // addParameter(param::ParameterFactory::declareParameterSet<int>("feature mix", featuremix));
+}
+
+void HOGTrainingRois::setup()
+{
+    //in_  = modifier_->addInput<TYPE>(LABEL);
+    //out_ = modifier_->addOutput<TYPE>(LABEL);
+}
+
+void HOGTrainingRois::process()
+{
+}
+
