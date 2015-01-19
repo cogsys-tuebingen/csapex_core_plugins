ebd18efc1062e85e340e299101e25ce5199c3215
Robert Pech <robert.pech@uni-tuebingen.de>
2015-01-19 15:53:34 +0100

    LSD line detector

---
+++ vision_plugins/src/features/lsd.cpp
@@ -0,0 +1,92 @@
+/*----------------------------------------------------------------------------
+
+  LSD - Line Segment Detector on digital images
+
+  Copyright (c) 2007-2011 rafael grompone von gioi <grompone@gmail.com>
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as
+  published by the Free Software Foundation, either version 3 of the
+  License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+  ----------------------------------------------------------------------------*/
+
+/// HEADER
+#include "lsd.h"
+
+/// PROJECT
+#include <csapex/utility/register_apex_plugin.h>
+#include <csapex/msg/input.h>
+#include <csapex/msg/output.h>
+#include <utils_param/parameter_factory.h>
+#include <csapex_vision/cv_mat_message.h>
+#include <csapex_core_plugins/vector_message.h>
+#include <csapex/model/node_modifier.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+
+using namespace csapex;
+using namespace csapex::connection_types;
+using namespace vision_plugins;
+
+CSAPEX_REGISTER_CLASS(vision_plugins::LineSegmentDetector, csapex::Node)
+
+
+LineSegmentDetector::LineSegmentDetector()
+{
+}
+
+void LineSegmentDetector::process()
+{
+    connection_types::CvMatMessage::Ptr a = input_->getMessage<connection_types::CvMatMessage>();
+
+//    cv::Mat image_cv = a->value;
+
+
+//    X = image_cv.rows;
+//    Y = image.cv.cols;
+//    /* get memory */
+
+//    image = new double [X*Y];
+//    if( image == NULL ) throw std::runtime_error("unsupported norm type");
+
+//    if(Image.type() == CV_8UC3) {
+//          Reading_image_impl<cv::Vec1b, Norm>(Image, Distance, Direction, Interpolation_coeff);
+//      } else if (Image.type() == CV_32FC3) {
+//          Reading_image_impl<cv::Vec1f, Norm>(Image, Distance, Direction, Interpolation_coeff);
+//      } else if (Image.type() == CV_64FC3) {
+//          Reading_image_impl<cv::Vec1d, Norm>(Image, Distance, Direction, Interpolation_coeff);
+//      } else {
+//          throw std::runtime_error("unsupported image type");
+//      }
+
+
+////  template <typename PixelType, template<typename> class Norm>
+////  void color_edge_detection::RCMG_computation_impl(cv::Mat Image, cv::Mat Distance, cv::Mat Direction, cv::Mat Interpolation_coeff)
+////  {
+///* read data */
+//for(int y=0;y<Y;y++)
+//  for(int x=0;x<X;x++)
+//    image[ x + y * X ] = (double) image_cv.at<image_cv.type()>(x,y);
+}
+
+void LineSegmentDetector::setup()
+{
+    input_ = modifier_->addInput<connection_types::CvMatMessage>("Image");
+
+    output_ = modifier_->addOutput<connection_types::CvMatMessage>("Image");
+}
+
+
---
+++ vision_plugins/src/features/lsd.h
@@ -0,0 +1,58 @@
+#ifndef LSD_H
+#define LSD_H
+
+/// COMPONENT
+#include "corner_line_detection.h"
+
+#define FIELD_LENGTH 160
+
+namespace vision_plugins{
+class LineSegmentDetector : public CornerLineDetection
+{
+public:
+
+    /** Structure to store one argument definition and read value.
+     */
+    struct argument
+    {
+      char name[FIELD_LENGTH];     /* name to internally identify the argument */
+      char desc[FIELD_LENGTH];     /* description */
+      char id;                     /* letter used with '-' to use the option */
+      char type;                   /* i=int, d=double, s=str, b=bool */
+      int required;
+      int assigned;
+      int def_value;               /* true or false, a default value is assigned? */
+      char d_value[FIELD_LENGTH];  /* default value */
+      char s_value[FIELD_LENGTH];  /* string found, also the value if 'str' */
+      int  i_value;
+      double f_value;
+      int    min_set;              /* true or false, is minimal value set? */
+      double min;
+      int    max_set;              /* true or false, is maximal value set? */
+      double max;
+    };
+
+    LineSegmentDetector();
+
+    virtual void process();
+    virtual void setup();
+
+    double * image;
+    int X,Y;
+    double * segs;
+    int n;
+    int dim = 7;
+    int * region;
+    int regX,regY;
+    int i,j;
+
+
+private:
+    csapex::Output* output_vector_;
+    csapex::Input*  input_;
+};
+}
+#endif // LSD_H
+
+
+
