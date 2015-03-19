530ccbde247c9d0f758c09144f039ad2421f43e3
Richard Hanten <richard.hanten@uni-tuebingen.de>
2015-03-19 18:30:56 +0100

    added mlp

---
+++ csapex_ml/src/mlp.h
@@ -0,0 +1,38 @@
+#ifndef MLP_NODE_H
+#define MLP_NODE_H
+
+/// COMPONENT
+#include <csapex_ml/features_message.h>
+#include "mlp/mlp.h"
+
+/// PROJECT
+#include <csapex/model/node.h>
+
+/// SYSTEM
+#include <mutex>
+
+namespace csapex {
+class MLP : public Node
+{
+public:
+    MLP();
+
+    virtual void setup(csapex::NodeModifier& node_modifier) override;
+    virtual void setupParameters(Parameterizable& parameters);
+    virtual void process() override;
+
+private:
+    Input        *in_;
+    Output       *out_;
+
+    std::mutex          m_;
+    mlp::MLP::Ptr       mlp_;
+    size_t              mlp_input_size_;
+    size_t              mlp_output_size_;
+    std::vector<int>    mlp_class_labels_;
+
+    void load();
+};
+}
+
+#endif // MLP_NODE_H
