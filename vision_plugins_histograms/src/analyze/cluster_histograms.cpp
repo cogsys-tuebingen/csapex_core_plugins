822c820b8d6a6435cc75e4827173cd7ea9347e73
Richard Hanten <richard.hanten@uni-tuebingen.de>
2015-05-19 19:26:32 +0200

    went on

--- vision_plugins_histograms/src/analyze/cluster_histograms.cpp
+++ vision_plugins_histograms/src/analyze/cluster_histograms.cpp
@@ -1,7 +1,74 @@
+/// HEADER
 #include "cluster_histograms.h"
+
+/// PROJECT
+#include <csapex/msg/io.h>
+#include <csapex_core_plugins/vector_message.h>
+#include <csapex_vision/cv_mat_message.h>
+#include <csapex/utility/register_apex_plugin.h>
+#include <utils_param/parameter_factory.h>
+#include <utils_vision/utils/histogram.hpp>
+#include <vision_plugins_histograms/histogram_msg.h>
+#include <csapex/model/node_modifier.h>
+#include <csapex/utility/assert.h>
+
+using namespace vision_plugins;
+using namespace csapex;
+using namespace csapex::connection_types;
+
+CSAPEX_REGISTER_CLASS(vision_plugins::ClusterHistograms, csapex::Node)
+
 
 ClusterHistograms::ClusterHistograms()
 {
 
 }
 
+void ClusterHistograms::process()
+{
+    CvMatMessage::ConstPtr in       = msg::getMessage<CvMatMessage>(input_);
+    CvMatMessage::ConstPtr clusters = msg::getMessage<CvMatMessage>(clusters_);
+    CvMatMessage::ConstPtr mask;
+    if(msg::hasMessage(mask_)) {
+        mask = msg::getMessage<CvMatMessage>(mask_);
+    }
+    HistogramMessage::Ptr             out_histograms(new HistogramMessage);
+    std::shared_ptr<std::vector<int>> out_labels(new std::vector<int>);
+
+    if(clusters->value.type() != CV_32SC1) {
+        throw std::runtime_error("Cluster label matrix must be single channel integer!");
+    }
+    if(!mask->value.empty() && mask->value.type() != CV_8UC1) {
+        throw std::runtime_error("Mask must be single channel uchar!");
+    }
+
+    /// PUT INTRESTING CODE HERE
+
+    msg::publish(out_histograms_, out_histograms);
+    msg::publish<GenericVectorMessage, int>(out_cluster_ids_, out_labels);
+
+}
+
+void ClusterHistograms::setup(NodeModifier &node_modifier)
+{
+    input_    = node_modifier.addInput<CvMatMessage>("input");
+    clusters_ = node_modifier.addInput<CvMatMessage>("labels");
+    mask_     = node_modifier.addInput<CvMatMessage>("mask");
+
+    out_cluster_ids_ = node_modifier.addOutput<GenericVectorMessage, int>("assignment");
+    out_histograms_  = node_modifier.addOutput<HistogramMessage>("histograms");
+
+    update();
+}
+
+void ClusterHistograms::setupParameters(Parameterizable &parameters)
+{
+    /// BINS
+    /// MIN MAX
+    /// ...
+}
+
+void ClusterHistograms::update()
+{
+
+}
--- vision_plugins_histograms/src/analyze/cluster_histograms.h
+++ vision_plugins_histograms/src/analyze/cluster_histograms.h
@@ -1,11 +1,32 @@
 #ifndef CLUSTERHISTOGRAMS_H
 #define CLUSTERHISTOGRAMS_H
 
+/// COMPONENT
+#include <csapex/model/node.h>
 
-class ClusterHistograms
+namespace vision_plugins {
+class ClusterHistograms : public csapex::Node
 {
 public:
     ClusterHistograms();
-};
 
+    virtual void process() override;
+    virtual void setup(csapex::NodeModifier& node_modifier) override;
+    virtual void setupParameters(Parameterizable& parameters);
+
+protected:
+    csapex::Output*   out_cluster_ids_;
+    csapex::Output*   out_histograms_;
+
+    csapex::Input*    input_;
+    csapex::Input*    clusters_;
+    csapex::Input*    mask_;
+
+    int bins_;
+
+
+    void update();
+
+};
+}
 #endif // CLUSTERHISTOGRAMS_H
