1525d1615e32c20037c456649d170eb1e4dbf259
buck <sebastian.buck@uni-tuebingen.de>
2015-07-21 02:07:35 +0200

    added message composition tools

---
+++ csapex_core_plugins/src/composer.cpp
@@ -0,0 +1,77 @@
+/// PROJECT
+#include <csapex/model/node.h>
+#include <csapex/utility/register_apex_plugin.h>
+#include <csapex/model/node_modifier.h>
+#include <csapex/msg/io.h>
+#include <utils_param/parameter_factory.h>
+#include <csapex_core_plugins/composite_message.h>
+
+namespace csapex
+{
+
+class Composer : public Node
+{
+public:
+    Composer()
+    {}
+
+    void setup(csapex::NodeModifier& node_modifier)
+    {
+        output_ = node_modifier.addOutput<connection_types::CompositeMessage>("Composed");
+    }
+
+    void setupParameters(Parameterizable& parameters)
+    {
+        addParameter(param::ParameterFactory::declareRange("inputs", 1, 10, 2, 1), [this](param::Parameter* p) {
+            updateInputs();
+        });
+    }
+
+    void process()
+    {
+        auto composite = std::make_shared<connection_types::CompositeMessage>();
+
+        for(auto input : modifier_->getMessageInputs()) {
+            ConnectionType::ConstPtr msg = msg::getMessage(input);
+            composite->value.push_back(msg);
+        }
+
+        msg::publish(output_, composite);
+    }
+
+    void updateInputs()
+    {
+        int input_count = readParameter<int>("inputs");
+
+        std::vector<Input*> inputs = modifier_->getMessageInputs();
+        int current_amount = inputs.size();
+
+        if(current_amount > input_count) {
+            for(int i = current_amount; i > input_count ; i--) {
+                Input* in = inputs[i - 1];
+                if(msg::isConnected(in)) {
+//                    msg::disable(in);
+                    break;
+                } else {
+                    modifier_->removeInput(msg::getUUID(in));
+                }
+            }
+        } else {
+            int to_add = input_count - current_amount;
+            for(int i = 0 ; i < current_amount; i++) {
+                msg::enable(inputs[i]);
+            }
+            for(int i = 0 ; i < to_add ; i++) {
+                modifier_->addOptionalInput<connection_types::AnyMessage>("Message");
+            }
+        }
+
+    }
+
+private:
+    Output* output_;
+};
+
+}
+
+CSAPEX_REGISTER_CLASS(csapex::Composer, csapex::Node)
---
+++ csapex_core_plugins/src/decomposer.cpp
@@ -0,0 +1,97 @@
+/// PROJECT
+#include <csapex/model/node.h>
+#include <csapex/utility/register_apex_plugin.h>
+#include <csapex/model/node_modifier.h>
+#include <csapex/msg/io.h>
+#include <utils_param/parameter_factory.h>
+#include <csapex_core_plugins/composite_message.h>
+#include <csapex/msg/output.h>
+#include <csapex/core/serialization.h>
+
+
+using namespace csapex;
+using namespace csapex::connection_types;
+
+namespace csapex
+{
+
+class Decomposer : public Node
+{
+    friend class DecomposerSerializer;
+
+public:
+    Decomposer()
+    {}
+
+    void setup(csapex::NodeModifier& node_modifier)
+    {
+        input_ = node_modifier.addInput<CompositeMessage>("Composed");
+    }
+
+    void setupParameters(Parameterizable& parameters)
+    {
+    }
+
+    void process()
+    {
+        CompositeMessage::ConstPtr message = msg::getMessage<CompositeMessage>(input_);
+
+        std::size_t n = message->value.size();
+
+        updateOutputs(n);
+
+        std::size_t i = 0;
+        for(ConnectionTypeConstPtr part : message->value) {
+            Output* o = outputs_[i++];
+
+            msg::setLabel(o, part->typeName());
+            o->setType(part->toType());
+
+            msg::publish(o, part);
+        }
+    }
+
+private:
+    void updateOutputs(std::size_t count)
+    {
+        if(count != outputs_.size()) {
+            for(std::size_t i = outputs_.size(); i < count; ++i) {
+                outputs_.push_back(modifier_->addOutput<AnyMessage>("Part"));
+            }
+        }
+    }
+
+private:
+    Input* input_;
+
+    std::vector<Output*> outputs_;
+};
+
+}
+
+CSAPEX_REGISTER_CLASS(csapex::Decomposer, csapex::Node)
+
+
+namespace csapex
+{
+class DecomposerSerializer
+{
+public:
+    static void serialize(const Decomposer& decomposer, YAML::Node& doc)
+    {
+        doc["channel_count"] = decomposer.outputs_.size();
+    }
+
+    static void deserialize(Decomposer& decomposer, const YAML::Node& doc)
+    {
+        int c = 0;
+        if(doc["channel_count"].IsDefined()) {
+            c = doc["channel_count"].as<int>();
+        }
+
+        decomposer.updateOutputs(c);
+    }
+};
+}
+
+CSAPEX_REGISTER_SERIALIZER(csapex::Decomposer, DecomposerSerializer)
