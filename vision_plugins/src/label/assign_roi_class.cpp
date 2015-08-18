d8100c8b0588c32dec87aba1e6d74bfe056afe35
buck <sebastian.buck@uni-tuebingen.de>
2015-08-18 15:17:19 +0200

    removed utils_qt

--- vision_plugins/src/label/assign_roi_class.cpp
+++ vision_plugins/src/label/assign_roi_class.cpp
@@ -3,7 +3,7 @@
 
 /// PROJECT
 #include <csapex/msg/io.h>
-#include <utils_qt/QtCvImageConverter.h>
+#include <csapex/view/QtCvImageConverter.h>
 #include <csapex/model/node_modifier.h>
 #include <csapex/utility/register_apex_plugin.h>
 #include <utils_param/parameter_factory.h>
