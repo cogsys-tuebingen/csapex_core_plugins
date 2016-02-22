dbdeec0f4ccb025cefd2532c8d3859346c5b2574
buck <sebastian.buck@uni-tuebingen.de>
2016-02-22 14:22:10 +0100

    cloud test node added

---
+++ csapex_point_cloud/src/cloud_test.cpp
@@ -0,0 +1,68 @@
+/// PROJECT
+#include <csapex/model/node.h>
+#include <csapex_point_cloud/point_cloud_message.h>
+#include <csapex/msg/io.h>
+#include <csapex_point_cloud/point_cloud_message.h>
+#include <csapex/param/parameter_factory.h>
+#include <csapex/signal/trigger.h>
+#include <csapex/model/node_modifier.h>
+#include <csapex/utility/register_apex_plugin.h>
+
+namespace csapex {
+
+using namespace connection_types;
+
+
+class CloudTest : public Node
+{
+public:
+    CloudTest()
+    {
+    }
+
+    virtual void setup(csapex::NodeModifier& node_modifier) override
+    {
+        input_cloud_ = node_modifier.addInput<PointCloudMessage>("PointCloud");
+
+        trigger_empty_ = node_modifier.addTrigger("empty");
+        trigger_not_empty_ = node_modifier.addTrigger("not empty");
+    }
+
+    virtual void setupParameters(Parameterizable &parameters) override
+    {
+        addParameter(param::ParameterFactory::declareRange("min count for not empty", 0, 1024, 1, 1), min_count_);
+    }
+
+    virtual void process() override
+    {
+        PointCloudMessage::ConstPtr msg(msg::getMessage<PointCloudMessage>(input_cloud_));
+        boost::apply_visitor (PointCloudMessage::Dispatch<CloudTest>(this, msg), msg->value);
+    }
+
+    template <class PointT>
+    void inputCloud(typename pcl::PointCloud<PointT>::ConstPtr cloud)
+    {
+        int count = 0;
+        for(const PointT& pt : cloud->points) {
+            if(pcl::isFinite(pt)) {
+                ++count;
+            }
+
+            if(count >= min_count_) {
+                trigger_not_empty_->trigger();
+                return;
+            }
+        }
+
+        trigger_empty_->trigger();
+    }
+
+private:
+    Input*  input_cloud_;
+    Trigger* trigger_empty_;
+    Trigger* trigger_not_empty_;
+
+    int min_count_;
+};
+CSAPEX_REGISTER_CLASS(csapex::CloudTest, csapex::Node)
+}
