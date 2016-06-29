b59aeab10fefbacbfca72f6c7085d547edddac10
buck <sebastian.buck@uni-tuebingen.de>
2016-06-29 16:53:03 +0200

    added stamp to segments

--- csapex_transform/src/time_offset.cpp
+++ csapex_transform/src/time_offset.cpp
@@ -20,7 +20,7 @@ TimeOffset::TimeOffset()
 
 void TimeOffset::setupParameters(Parameterizable &parameters)
 {
-    parameters.addParameter(csapex::param::ParameterFactory::declareRange<double>("offset", -5000.0, 5000.0, 0.0, 0.5));
+    parameters.addParameter(csapex::param::ParameterFactory::declareRange<double>("offset", -5.0, 5.0, 0.0, 0.001));
 }
 
 
