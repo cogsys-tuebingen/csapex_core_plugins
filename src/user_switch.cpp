c852b7628818e3a7a742319fa661675c9db54ccd
buck <sebastian.buck@uni-tuebingen.de>
2016-08-15 02:26:44 +0200

    moved cv mat message to a new package

--- src/user_switch.cpp
+++ src/user_switch.cpp
@@ -2,7 +2,7 @@
 #include <csapex/model/node.h>
 #include <csapex/csapex_fwd.h>
 #include <csapex/model/node_modifier.h>
-#include <csapex_vision/cv_mat_message.h>
+#include <csapex_opencv/cv_mat_message.h>
 #include <csapex/msg/io.h>
 #include <csapex/param/parameter_factory.h>
 #include <csapex_tutorial/tutorial_message.h>
