3d68f9be52ac1cf1cb3727cb894e4c3ba33b9b98
Richard Hanten <Richard.Hanten@uni-tuebingen.de>
2016-08-21 00:28:59 +0200

    added roi evaluation node

---
+++ csapex_vision/src/roi/evaluate_roi_classification.cpp
@@ -0,0 +1,74 @@
+/// PROJECT
+#include <csapex/model/node.h>
+#include <csapex/msg/generic_vector_message.hpp>
+#include <csapex_ml/features_message.h>
+#include <csapex_opencv/roi_message.h>
+#include <csapex_evaluation/confusion_matrix_message.h>
+#include <csapex/msg/io.h>
+#include <csapex/param/parameter_factory.h>
+#include <csapex/model/node_modifier.h>
+#include <csapex/utility/register_apex_plugin.h>
+
+namespace csapex {
+
+using namespace connection_types;
+
+class EvaluateROIClassification : public csapex::Node
+{
+public:
+    EvaluateROIClassification()
+    {
+    }
+
+    virtual void setup(csapex::NodeModifier& node_modifier) override
+    {
+        node_modifier.addInput<GenericVectorMessage, RoiMessage>("Ground Truth");
+        node_modifier.addInput<GenericVectorMessage, RoiMessage>("Predicted");
+        node_modifier.addOutput<ConfusionMatrixMessage>("Confusion");
+
+        node_modifier.addSlot("Reset",
+                              std::bind(&EvaluateROIClassification::reset, this));
+    }
+    virtual void setupParameters(Parameterizable &parameters) override
+    {
+        parameters.addParameter(param::ParameterFactory::declareTrigger("/reset"),
+                                std::bind(&EvaluateROIClassification::reset, this));
+
+    }
+    virtual void process() override
+    {
+        std::shared_ptr<std::vector<RoiMessage> const> input_gt =
+                msg::getMessage<GenericVectorMessage, RoiMessage>(input_gt_);
+        std::shared_ptr<std::vector<RoiMessage> const> input_prediction =
+                msg::getMessage<GenericVectorMessage, RoiMessage>(input_prediction_);
+
+        ConfusionMatrixMessage::Ptr output_confusion(new ConfusionMatrixMessage);
+
+        if(input_gt->size() != input_prediction->size())
+            throw std::runtime_error("Size of ground truth data must be the same as predicted vice versa!");
+
+        const std::size_t size = input_gt->size();
+        for(std::size_t i = 0 ; i < size ; ++i) {
+            const RoiMessage &gt = input_gt->at(i);
+            const RoiMessage &prediction = input_prediction->at(i);
+            output_confusion->confusion.reportClassification(gt.value.classification(),
+                                                             prediction.value.classification());
+        }
+        msg::publish(output_confusion_, output_confusion);
+    }
+
+private:
+    Input* input_gt_;
+    Input* input_prediction_;
+
+    Output* output_confusion_;
+
+    void reset()
+    {
+
+    }
+
+};
+
+}
+CSAPEX_REGISTER_CLASS(csapex::EvaluateROIClassification, csapex::Node)
