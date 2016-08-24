164091ed0af28d8583c389f91a9c6b4d389b2dd7
Richard Hanten <Richard.Hanten@uni-tuebingen.de>
2016-08-24 21:44:18 +0200

    tiny extension

--- csapex_vision/src/color/lab_to_lch.cpp
+++ csapex_vision/src/color/lab_to_lch.cpp
@@ -15,11 +15,11 @@ class LABToLCh : public csapex::Node
 public:
     LABToLCh()
     {
-        LUT_sqrt = cv::Mat(255, 255, CV_32FC1, cv::Scalar());
-        LUT_atan2 = cv::Mat(255, 255, CV_32FC1, cv::Scalar());
+        LUT_sqrt_ = cv::Mat(255, 255, CV_32FC1, cv::Scalar());
+        LUT_atan2_ = cv::Mat(255, 255, CV_32FC1, cv::Scalar());
 
-        float *LUT_sqrt_ptr = LUT_sqrt.ptr<float>();
-        float *LUT_atan2_ptr = LUT_atan2.ptr<float>();
+        float *LUT_sqrt_ptr = LUT_sqrt_.ptr<float>();
+        float *LUT_atan2_ptr = LUT_atan2_.ptr<float>();
         for(int i = 0 ; i < 255 ; ++i) {
             for(int j = 0 ; j < 255 ; ++j) {
                 const float a = i - 128.f;
@@ -56,7 +56,10 @@ public:
             processCV_8UC3(in->value, out->value);
             break;
         case CV_32FC3:
-            processCV_32FC3(in->value, out->value);
+            if(float_use_lut_)
+                processCV_32FC3(in->value, out->value);
+            else
+                processCV_32FC3NoLUT(in->value, out->value);
             break;
         default:
             throw std::runtime_error("Unsupported image depth!");
@@ -73,17 +76,22 @@ public:
 
     virtual void setupParameters(Parameterizable &parameters)
     {
+        parameters.addParameter(param::ParameterFactory::declareBool("/float/use_lut", true),
+                                float_use_lut_);
     }
 
 protected:
     csapex::Output*   output_;
     csapex::Input*    input_;
 
-    static constexpr float NORM_L = 1.f / 255.f;
+    static constexpr float NORM_L_UC = 1.f / 255.f;
+    static constexpr float NORM_L_FC = 1.f / 100.f;
     static constexpr float NORM_C = 1.f / (127.0 * sqrt(2.0));
     static constexpr float NORM_H = 1.f / (2 * M_PI);
-    cv::Mat LUT_sqrt;
-    cv::Mat LUT_atan2;
+    cv::Mat LUT_sqrt_;
+    cv::Mat LUT_atan2_;
+    bool    float_use_lut_;
+
 
     inline void processCV_8UC3(const cv::Mat &src, cv::Mat &dst)
     {
@@ -91,12 +99,12 @@ protected:
 
         float *dst_ptr = dst.ptr<float>();
         uchar const *src_ptr = src.ptr<uchar>();
-        const float *LUT_sqrt_ptr = LUT_sqrt.ptr<float>();
-        const float *LUT_atan2_ptr = LUT_atan2.ptr<float>();
+        const float *LUT_sqrt_ptr = LUT_sqrt_.ptr<float>();
+        const float *LUT_atan2_ptr = LUT_atan2_.ptr<float>();
 
         const std::size_t size = src.rows * src.cols;
         for(std::size_t i = 0 ; i < size ; ++i) {
-            dst_ptr[0] = src_ptr[0] * NORM_L;
+            dst_ptr[0] = src_ptr[0] * NORM_L_UC;
             const int a = src_ptr[1];
             const int b = src_ptr[2];
             dst_ptr[1] = LUT_sqrt_ptr[255 * a + b];
@@ -112,18 +120,52 @@ protected:
 
         float *dst_ptr = dst.ptr<float>();
         float const *src_ptr = src.ptr<float>();
-        const float *LUT_sqrt_ptr = LUT_sqrt.ptr<float>();
-        const float *LUT_atan2_ptr = LUT_atan2.ptr<float>();
+        const float *LUT_sqrt_ptr = LUT_sqrt_.ptr<float>();
+        const float *LUT_atan2_ptr = LUT_atan2_.ptr<float>();
 
         const std::size_t size = src.rows * src.cols;
         for(std::size_t i = 0 ; i < size ; ++i) {
-            dst_ptr[0] = src_ptr[0] * NORM_L;
+            dst_ptr[0] = src_ptr[0] * NORM_L_FC;
             const int a = src_ptr[1] + 128.f;
             const int b = src_ptr[2] + 128.f;
             dst_ptr[1] = LUT_sqrt_ptr[255 * a + b];
             dst_ptr[2] = LUT_atan2_ptr[255 * a + b];
             dst_ptr += 3;
             src_ptr += 3;
+        }
+    }
+
+    inline void processCV_32FC3NoLUT(const cv::Mat &src, cv::Mat &dst)
+    {
+
+        dst = cv::Mat(src.rows, src.cols, CV_32FC3, cv::Scalar());
+        float const *src_ptr = src.ptr<float>();
+        float *dst_ptr = dst.ptr<float>();
+
+        const std::size_t size = src.rows * src.cols;
+        for(std::size_t i = 0 ; i < size ; ++i) {
+            dst_ptr[0]  = src_ptr[0] * NORM_L_FC;
+            const float a = (src_ptr[1] - 128.f); /// opencv specific offset for 8UC1
+            const float b = (src_ptr[2] - 128.f); /// opencv specific offset for 8UC1
+
+            if(a != 0.f && b != 0.f) {
+                dst_ptr[1] = sqrt(a * a + b * b)  * NORM_C;
+                dst_ptr[2] = (atan2(b, a) + M_PI) * NORM_H;
+            } else {
+                if(a != 0) {
+                    dst_ptr[1] = fabs(a) * NORM_C;
+                    if(a < 0)
+                        dst_ptr[2] = 1.f;
+                } else if(b != 0){
+                    dst_ptr[1] = fabs(b) * NORM_C;
+                    if(b > 0)
+                        dst_ptr[2] = 0.75f;
+                    else
+                        dst_ptr[2] = 0.25f;
+                }
+            }
+            src_ptr += 3;
+            dst_ptr += 3;
         }
     }
 
