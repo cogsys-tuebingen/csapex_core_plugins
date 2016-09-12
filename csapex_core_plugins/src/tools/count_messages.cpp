c62ac785149ea583eaed195fb968eebe7f973138
Richard Hanten <Richard.Hanten@uni-tuebingen.de>
2016-09-12 21:30:49 +0200

    counting messages tools

---
+++ csapex_core_plugins/src/tools/count_messages.cpp
@@ -0,0 +1,61 @@
+/// PROJECT
+#include <csapex/msg/io.h>
+#include <csapex/model/token_data.h>
+#include <csapex/msg/message.h>
+#include <csapex/model/node_modifier.h>
+#include <csapex/utility/register_apex_plugin.h>
+#include <csapex/msg/any_message.h>
+#include <csapex/msg/generic_value_message.hpp>
+#include <csapex/param/parameter_factory.h>
+#include <csapex/model/node.h>
+#include <csapex/model/token_data.h>
+#include <csapex/signal/slot.h>
+
+namespace csapex {
+
+class CSAPEX_EXPORT_PLUGIN CountMessages : public Node
+{
+public:
+    CountMessages()
+        : input_(nullptr), output_(nullptr), reset_(nullptr), count_(0)
+    {
+    }
+
+
+    virtual void setup(csapex::NodeModifier& node_modifier) override
+    {
+        input_ = node_modifier.addInput<connection_types::AnyMessage>("Anything");
+        output_ = node_modifier.addOutput<std::size_t>("Count");
+        reset_ = node_modifier.addSlot("Reset",
+                                            std::bind(&CountMessages::reset, this));
+    }
+
+    virtual void setupParameters(Parameterizable &parameters) override
+    {
+        parameters.addParameter(param::ParameterFactory::declareTrigger("reset"),
+                                std::bind(&CountMessages::reset, this));
+    }
+
+    virtual void process() override
+    {
+        ++count_;
+        msg::publish(output_, count_);
+    }
+
+private:
+    Input* input_;
+    Output* output_;
+    Slot*   reset_;
+
+    std::size_t count_;
+
+    void reset()
+    {
+        count_ = 0;
+    }
+
+};
+
+}
+
+CSAPEX_REGISTER_CLASS(csapex::CountMessages, csapex::Node)
