3aebe310dde56410d1e4076884bc5263dd3387b8
Adrian Zwiener <adrian.zwiener@uni-tuebingen.de>
2016-11-14 22:24:10 +0100

    point cloud to image and reverse, correct point cloud headers

--- csapex_point_cloud/src/conversion/pointcloud_to_pointmatrix.cpp
+++ csapex_point_cloud/src/conversion/pointcloud_to_pointmatrix.cpp
@@ -38,21 +38,27 @@ void PointCloudToPointMatrix::setup(NodeModifier& node_modifier)
 namespace implementation {
 template<class PointT>
 struct Impl {
-    static void convert(const typename pcl::PointCloud<PointT>::ConstPtr cloud, cv::Mat &matrix, cv::Mat &mask)
+    static void convert(const typename pcl::PointCloud<PointT>::ConstPtr cloud,
+                        CvMatMessage::Ptr &matrix,
+                        CvMatMessage::Ptr &mask)
     {
+        matrix->setEncoding(enc::pointXYZ);
+
         int height = cloud->height;
         int width  = cloud->width;
-        matrix = cv::Mat(height, width, CV_32FC3);
-        mask   = cv::Mat(height, width, CV_8UC1, 255);
+        cv::Mat &matrix_value = matrix->value;
+        cv::Mat &mask_value   = mask->value;
+        matrix_value = cv::Mat(height, width, CV_32FC3);
+        mask_value   = cv::Mat(height, width, CV_8UC1, 255);
 
         for(int i = 0 ; i < height ; ++i) {
             for(int j = 0 ; j < width ; ++j) {
                 PointT pos = cloud->at(i * width + j);
-                matrix.at<float>(i, (j * 3 + 0)) = pos.x;
-                matrix.at<float>(i, (j * 3 + 1)) = pos.y;
-                matrix.at<float>(i, (j * 3 + 2)) = pos.z;
+                matrix_value.at<float>(i, (j * 3 + 0)) = pos.x;
+                matrix_value.at<float>(i, (j * 3 + 1)) = pos.y;
+                matrix_value.at<float>(i, (j * 3 + 2)) = pos.z;
                 if(pos.x == 0.f && pos.y == 0.f && pos.z == 0.f) {
-                    mask.at<uchar>(i,j) = 0;
+                    mask_value.at<uchar>(i,j) = 0;
                 }
             }
         }
@@ -60,7 +66,9 @@ struct Impl {
 };
 template <>
 struct Impl<pcl::PointXY> {
-    static void convert(const typename pcl::PointCloud<pcl::PointXY>::ConstPtr cloud, cv::Mat &matrix, cv::Mat &mask)
+    static void convert(const typename pcl::PointCloud<pcl::PointXY>::ConstPtr cloud,
+                        CvMatMessage::Ptr &matrix,
+                        CvMatMessage::Ptr &mask)
     {
         std::runtime_error("Conversion is not supported for pcl::PointXY!");
     }
@@ -68,22 +76,61 @@ struct Impl<pcl::PointXY> {
 
 template<>
 struct Impl<pcl::PointXYZI> {
-    static void convert(const typename pcl::PointCloud<pcl::PointXYZI>::ConstPtr cloud, cv::Mat &matrix, cv::Mat &mask)
+    static void convert(const typename pcl::PointCloud<pcl::PointXYZI>::ConstPtr cloud,
+                        CvMatMessage::Ptr &matrix,
+                        CvMatMessage::Ptr &mask)
     {
+        matrix->setEncoding(enc::pointXYZI);
+
         int height = cloud->height;
         int width  = cloud->width;
-        matrix = cv::Mat(height, width, CV_32FC4);
-        mask   = cv::Mat(height, width, CV_8UC1, 255);
+        cv::Mat &matrix_value = matrix->value;
+        cv::Mat &mask_value   = mask->value;
+
+        matrix_value = cv::Mat(height, width, CV_32FC4);
+        mask_value   = cv::Mat(height, width, CV_8UC1, 255);
 
         for(int i = 0 ; i < height ; ++i) {
             for(int j = 0 ; j < width ; ++j) {
                 pcl::PointXYZI pos = cloud->at(i * width + j);
-                matrix.at<float>(i, (j * 4 + 0)) = pos.x;
-                matrix.at<float>(i, (j * 4 + 1)) = pos.y;
-                matrix.at<float>(i, (j * 4 + 2)) = pos.z;
-                matrix.at<float>(i, (j * 4 + 3)) = pos.intensity;
+                matrix_value.at<float>(i, (j * 4 + 0)) = pos.x;
+                matrix_value.at<float>(i, (j * 4 + 1)) = pos.y;
+                matrix_value.at<float>(i, (j * 4 + 2)) = pos.z;
+                matrix_value.at<float>(i, (j * 4 + 3)) = pos.intensity;
                 if(pos.x == 0.f && pos.y == 0.f && pos.z == 0.f) {
-                    mask.at<uchar>(i,j) = 0;
+                    mask_value.at<uchar>(i,j) = 0;
+                }
+            }
+        }
+    }
+};
+
+template<>
+struct Impl<pcl::PointXYZRGB> {
+    static void convert(const typename pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr cloud,
+                        CvMatMessage::Ptr &matrix,
+                        CvMatMessage::Ptr &mask)
+    {
+        matrix->setEncoding(enc::pointXYZRGB);
+        int height = cloud->height;
+        int width  = cloud->width;
+        cv::Mat &matrix_value = matrix->value;
+        cv::Mat &mask_value   = mask->value;
+
+        matrix_value = cv::Mat(height, width, CV_32FC(6), cv::Scalar());
+        mask_value   = cv::Mat(height, width, CV_8UC1, 255);
+
+        for(int i = 0 ; i < height ; ++i) {
+            for(int j = 0 ; j < width ; ++j) {
+                pcl::PointXYZRGB pos = cloud->at(i * width + j);
+                matrix_value.at<float>(i, (j * 6 + 0)) = pos.x;
+                matrix_value.at<float>(i, (j * 6 + 1)) = pos.y;
+                matrix_value.at<float>(i, (j * 6 + 2)) = pos.z;
+                matrix_value.at<float>(i, (j * 6 + 3)) = pos.r;
+                matrix_value.at<float>(i, (j * 6 + 4)) = pos.g;
+                matrix_value.at<float>(i, (j * 6 + 5)) = pos.b;
+                if(pos.x == 0.f && pos.y == 0.f && pos.z == 0.f) {
+                    mask_value.at<uchar>(i,j) = 0;
                 }
             }
         }
@@ -98,7 +145,9 @@ void PointCloudToPointMatrix::inputCloud(typename pcl::PointCloud<PointT>::Const
 {
     CvMatMessage::Ptr out(new CvMatMessage(enc::unknown, cloud->header.stamp));
     CvMatMessage::Ptr mask(new CvMatMessage(enc::mono, cloud->header.stamp));
-    implementation::Impl<PointT>::convert(cloud, out->value, mask->value);
+    out->frame_id = cloud->header.frame_id;
+    mask->frame_id = cloud->header.frame_id;
+    implementation::Impl<PointT>::convert(cloud, out, mask);
     msg::publish(output_, out);
     msg::publish(mask_, mask);
 }
