c13f3576fa60581eab67dc0986246444ff621d77
Adrian Zwiener <adrian.zwiener@uni-tuebingen.de>
2017-01-26 02:30:10 +0100

    created adapter for parameter picking. does not work yet

--- csapex_core_plugins/src/tools/create_map_message.cpp
+++ csapex_core_plugins/src/tools/create_map_message.cpp
@@ -1,4 +1,4 @@
-
+#include "create_map_message.h"
 /// PROJECT
 #include <csapex/model/node.h>
 #include <csapex/utility/register_apex_plugin.h>
@@ -13,89 +13,74 @@
 using namespace csapex;
 using namespace csapex::connection_types;
 
-namespace csapex
-{
-
-
-class CSAPEX_EXPORT_PLUGIN CreateMapMessage : public Node, public VariadicInputs
-{
-public:
-    CreateMapMessage()
-    {
-    }
-
-    void setup(csapex::NodeModifier& modifier) override
-    {
-        setupVariadic(modifier);
-
-        out_ = modifier.addOutput<MapMessage>("map");
-    }
-
-    void setupParameters(csapex::Parameterizable& params) override
-    {
-        setupVariadicParameters(params);
-    }
-
-    void process() override
-    {
-       MapMessage::Ptr result(new MapMessage);
-
-        std::vector<InputPtr> inputs = node_modifier_->getMessageInputs();
-        for(std::size_t i = 0 ; i < inputs.size() ; i++) {
-            Input *in = inputs[i].get();
-            if(msg::hasMessage(in)) {
-                TokenData::ConstPtr m =  msg::getMessage(in);
-
-                if(keys_.size() > i){
-                    KeyValueMessage tmp(keys_[i],m);
-                    result->value.emplace_back(tmp);
-                }
-            }
-        }
-
-        msg::publish(out_, result);
-    }
-
-    virtual csapex::Input* createVariadicInput(csapex::TokenDataConstPtr type, const std::string& label, bool optional) override
-    {
-        std::size_t i = getVariadicInputCount();
-        std::string param_name = std::string("key_") + std::to_string(i);
-
-        param::Parameter::Ptr key_param;
-        if(hasParameter(param_name)) {
-            key_param = getParameter(param_name);
-        } else  {
-            key_param = param::ParameterFactory::declareText(param_name, "name");
-        }
-
-        keys_.push_back("");
-
-        std::function<void(param::Parameter* p)> cb = [this,i](param::Parameter* p){
-            if(p->is<std::string>()){
-                keys_.at(i)=(p->as<std::string>());
-            }
-        };
-        if(!key_param) {
-            throw std::runtime_error("Could not create temporary parameter!");
-        }
-
-        params_keys_.emplace_back(key_param);
-        addPersistentParameter(key_param);
-        addParameterCallback(key_param, cb);
-
-        return VariadicInputs::createVariadicInput(connection_types::makeEmpty<connection_types::AnyMessage>(), label.empty() ? "Value" : label, getVariadicInputCount() == 0 ? false : true);
-    }
-
-private:
-    Input* in_;
-    Output* out_;
-
-    std::vector<param::Parameter::Ptr> params_keys_;
-    std::vector<std::string> keys_;
-
-};
-
-} // csapex
-
 
 CSAPEX_REGISTER_CLASS(csapex::CreateMapMessage, csapex::Node)
+
+CreateMapMessage::CreateMapMessage()
+{
+}
+
+void CreateMapMessage::setup(csapex::NodeModifier& modifier)
+{
+    setupVariadic(modifier);
+
+    out_ = modifier.addOutput<MapMessage>("map");
+}
+
+void CreateMapMessage::setupParameters(csapex::Parameterizable& params)
+{
+    setupVariadicParameters(params);
+}
+
+void CreateMapMessage::process()
+{
+    MapMessage::Ptr result(new MapMessage);
+
+    std::vector<InputPtr> inputs = node_modifier_->getMessageInputs();
+    for(std::size_t i = 0 ; i < inputs.size() ; i++) {
+        Input *in = inputs[i].get();
+        if(msg::hasMessage(in)) {
+            TokenData::ConstPtr m =  msg::getMessage(in);
+
+            if(keys_.size() > i){
+                KeyValueMessage tmp(keys_[i],m);
+                result->value.emplace_back(tmp);
+            }
+        }
+    }
+
+    msg::publish(out_, result);
+}
+
+csapex::Input* CreateMapMessage::createVariadicInput(csapex::TokenDataConstPtr type, const std::string& label, bool optional)
+{
+    std::size_t i = getVariadicInputCount();
+    std::string param_name = std::string("key_") + std::to_string(i);
+
+    param::Parameter::Ptr key_param;
+    if(hasParameter(param_name)) {
+        key_param = getParameter(param_name);
+    } else  {
+        key_param = param::ParameterFactory::declareText(param_name, "name");
+    }
+
+    keys_.push_back("");
+
+    std::function<void(param::Parameter* p)> cb = [this,i](param::Parameter* p){
+        if(p->is<std::string>()){
+            keys_.at(i)=(p->as<std::string>());
+        }
+    };
+    if(!key_param) {
+        throw std::runtime_error("Could not create temporary parameter!");
+    }
+
+    params_keys_.emplace_back(key_param);
+    addPersistentParameter(key_param);
+    addParameterCallback(key_param, cb);
+
+    return VariadicInputs::createVariadicInput(connection_types::makeEmpty<connection_types::AnyMessage>(), label.empty() ? "Value" : label, getVariadicInputCount() == 0 ? false : true);
+}
+
+
+
---
+++ csapex_core_plugins/src/tools/parameter_dialog.cpp
@@ -0,0 +1,82 @@
+/// HEADER
+#include "parameter_dialog.h"
+
+/// PROJECT
+#include <csapex/param/parameter.h>
+#include <csapex/param/parameter_factory.h>
+
+/// SYSTEM
+#include <QDialogButtonBox>
+#include <QBoxLayout>
+#include <QFormLayout>
+#include <QDoubleSpinBox>
+#include <QLineEdit>
+#include <limits>
+
+ParameterDialog::ParameterDialog(const std::string& type, QWidget *parent, Qt::WindowFlags f)
+    : QDialog(parent, f), type_(type)
+{
+    makeUI();
+}
+
+void ParameterDialog::makeUI()
+{
+    QVBoxLayout* layout = new QVBoxLayout;
+
+    QFormLayout* form = new QFormLayout;
+
+    name = new QLineEdit;
+    name->setText("param");
+    form->addRow("name", name);
+
+    min = new QDoubleSpinBox;
+    min->setMinimum(-std::numeric_limits<double>::max());
+    min->setMaximum(std::numeric_limits<double>::max());
+    min->setDecimals(10);
+    form->addRow("minimum value", min);
+
+    max = new QDoubleSpinBox;
+    max->setMinimum(-std::numeric_limits<double>::max());
+    max->setMaximum(std::numeric_limits<double>::max());
+    max->setValue(1.0);
+    max->setDecimals(10);
+    form->addRow("maximum value", max);
+
+    step = new QDoubleSpinBox;
+    step->setMinimum(0);
+    step->setMaximum(std::numeric_limits<double>::max());
+    step->setValue(0.1);
+    step->setDecimals(10);
+    form->addRow("step_size", step);
+
+    layout->addLayout(form);
+
+    QDialogButtonBox* buttons = new QDialogButtonBox;
+    buttons->setStandardButtons(QDialogButtonBox::Ok | QDialogButtonBox::Cancel);
+    layout->addWidget(buttons);
+
+
+    setWindowTitle(QString("Create RangeParam<") + type_.c_str()  + ">");
+    setLayout(layout);
+    setModal(true);
+
+    QObject::connect(buttons, SIGNAL(accepted()), this, SLOT(finish()));
+    QObject::connect(buttons, SIGNAL(rejected()), this, SLOT(reject()));
+}
+
+csapex::param::Parameter::Ptr ParameterDialog::getParameter()
+{
+    return param_;
+}
+
+void ParameterDialog::finish()
+{
+    param_ = csapex::param::ParameterFactory::declareRange<double>(name->text().toStdString(),
+                min->value(), max->value(), min->value(), step->value());
+    if(param_) {
+        Q_EMIT accept();
+    } else {
+        Q_EMIT reject();
+    }
+}
+
