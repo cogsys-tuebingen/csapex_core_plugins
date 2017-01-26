e37feb1027bd4d084e94b4ee46a282269e16d2d6
Adrian Zwiener <adrian.zwiener@uni-tuebingen.de>
2017-01-26 17:07:23 +0100

    missing header

---
+++ csapex_core_plugins/include/csapex_core_plugins/parameter_dialog.h
@@ -0,0 +1,41 @@
+#ifndef PARAMETERDIALOG_H
+#define PARAMETERDIALOG_H
+
+
+/// PROJECT
+#include <csapex/param/param_fwd.h>
+
+/// SYSTEM
+#include <QDialog>
+
+class QLineEdit;
+class QDoubleSpinBox;
+
+class ParameterDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    ParameterDialog(const std::string &type, QWidget *parent = 0, Qt::WindowFlags f = 0);
+
+    csapex::param::ParameterPtr getParameter();
+
+private Q_SLOTS:
+    void finish();
+
+private:
+    void makeUI();
+
+private:
+    std::string type_;
+
+    QLineEdit* name;
+    QDoubleSpinBox* min;
+    QDoubleSpinBox* max;
+    QDoubleSpinBox* step;
+
+    csapex::param::ParameterPtr param_;
+};
+
+
+#endif // PARAMETERDIALOG_H
