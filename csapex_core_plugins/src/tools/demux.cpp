18d3206ccf529b66bb583cec811bf2eaf40d4689
Philipp Kuhlmann <philipp.kuhlmann@student.uni-tuebingen.de>
2017-01-28 01:01:29 +0100

    core: add mux/demux nodes

---
+++ csapex_core_plugins/src/tools/demux.cpp
@@ -0,0 +1,54 @@
+/// PROJECT
+#include <csapex/model/node.h>
+#include <csapex/utility/register_apex_plugin.h>
+#include <csapex/model/node_modifier.h>
+#include <csapex/model/variadic_io.h>
+#include <csapex/msg/io.h>
+#include <csapex/msg/any_message.h>
+#include <csapex/msg/generic_value_message.hpp>
+
+namespace csapex
+{
+
+class Demux : public Node, public VariadicOutputs
+{
+public:
+    Demux()
+    {}
+
+    void setup(csapex::NodeModifier& node_modifier)
+    {
+        selector_ = node_modifier.addInput<connection_types::GenericValueMessage<int>>("select");
+        input_ = node_modifier.addInput<connection_types::AnyMessage>("message");
+
+        VariadicOutputs::setupVariadic(node_modifier);
+    }
+
+    void setupParameters(Parameterizable& parameters)
+    {
+        VariadicOutputs::setupVariadicParameters(parameters);
+    }
+
+    void process()
+    {
+        int select = msg::getValue<int>(selector_);
+        if (select < 0 || select >= static_cast<int>(variadic_outputs_.size()))
+            throw std::runtime_error(std::string("demux index out of range: ") + std::to_string(select));
+
+        TokenData::ConstPtr msg = msg::getMessage<TokenData>(input_);
+        msg::publish(variadic_outputs_[select].get(), msg);
+    }
+
+    virtual csapex::Output* createVariadicOutput(csapex::TokenDataConstPtr type, const std::string& label) override
+    {
+        return VariadicOutputs::createVariadicOutput(connection_types::makeEmpty<connection_types::AnyMessage>(), "message");
+    }
+
+private:
+    Input* selector_;
+    Input* input_;
+};
+
+}
+
+CSAPEX_REGISTER_CLASS(csapex::Demux, csapex::Node)
---
+++ csapex_core_plugins/src/tools/mux.cpp
@@ -0,0 +1,69 @@
+/// PROJECT
+#include <csapex/model/node.h>
+#include <csapex/utility/register_apex_plugin.h>
+#include <csapex/model/node_modifier.h>
+#include <csapex/model/variadic_io.h>
+#include <csapex/msg/io.h>
+#include <csapex/msg/any_message.h>
+#include <csapex/msg/generic_value_message.hpp>
+
+namespace csapex
+{
+
+class Mux : public Node, public VariadicInputs
+{
+public:
+    Mux()
+    {}
+
+    void setup(csapex::NodeModifier& node_modifier)
+    {
+        selector_ = node_modifier.addOptionalInput<connection_types::GenericValueMessage<int>>("select");
+        VariadicInputs::setupVariadic(node_modifier);
+
+        output_ = node_modifier.addOutput<connection_types::AnyMessage>("message");
+    }
+
+    void setupParameters(Parameterizable& parameters)
+    {
+        VariadicInputs::setupVariadicParameters(parameters);
+    }
+
+    void process()
+    {
+        TokenData::ConstPtr msg;
+
+        if (msg::hasMessage(selector_)) {
+            int select = msg::getValue<int>(selector_);
+            if (select < 0 || select >= static_cast<int>(variadic_inputs_.size()))
+                throw std::runtime_error(std::string("mux index out of range: ") + std::to_string(select));
+
+            msg = msg::getMessage<TokenData>(variadic_inputs_[select].get());
+        } else {
+            for (auto input : variadic_inputs_) {
+                if (msg::hasMessage(input.get())) {
+                    if (!msg)
+                        msg = msg::getMessage<TokenData>(input.get());
+                    else
+                        throw std::runtime_error("more than one message available without explicit selection");
+                }
+            }
+        }
+
+        if (msg)
+            msg::publish(output_, msg);
+    }
+
+    virtual csapex::Input* createVariadicInput(csapex::TokenDataConstPtr type, const std::string& label, bool optional) override
+    {
+        return VariadicInputs::createVariadicInput(connection_types::makeEmpty<connection_types::AnyMessage>(), "message", true);
+    }
+
+private:
+    Input* selector_;
+    Output* output_;
+};
+
+}
+
+CSAPEX_REGISTER_CLASS(csapex::Mux, csapex::Node)
