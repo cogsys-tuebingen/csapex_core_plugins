9822da096d3de9dcd4d32a25a2250671a53d425b
buck <sebastian.buck@uni-tuebingen.de>
2017-02-23 00:56:52 +0100

    various fixes

---
+++ csapex_core_plugins/src/tools/vector_size.cpp
@@ -0,0 +1,56 @@
+
+/// PROJECT
+#include <csapex/model/node.h>
+#include <csapex/msg/io.h>
+#include <csapex/param/parameter_factory.h>
+#include <csapex/model/node_modifier.h>
+#include <csapex/utility/register_apex_plugin.h>
+#include <csapex/msg/generic_vector_message.hpp>
+
+using namespace csapex;
+using namespace csapex::connection_types;
+
+namespace csapex
+{
+
+
+class VectorSize : public Node
+{
+public:
+    VectorSize()
+    {
+    }
+
+    void setup(csapex::NodeModifier& modifier) override
+    {
+        in_ = modifier.addInput<GenericVectorMessage>("Input");
+        out_ = modifier.addOutput<int>("Output");
+    }
+
+    void setupParameters(csapex::Parameterizable& params) override
+    {
+        params.addParameter(param::ParameterFactory::declareOutputText("count"));
+    }
+
+    void process() override
+    {
+        GenericVectorMessage::ConstPtr vec = msg::getMessage<GenericVectorMessage>(in_);
+
+        int size = vec->nestedValueCount();
+
+        setParameter("count", std::to_string(size));
+
+        msg::publish(out_, size);
+    }
+
+private:
+    Input* in_;
+    Output* out_;
+
+};
+
+} // csapex
+
+
+CSAPEX_REGISTER_CLASS(csapex::VectorSize, csapex::Node)
+
