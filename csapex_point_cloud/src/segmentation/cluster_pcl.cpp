c6fd3a35123226ccb0dee633b62eea3005e9aa91
Richard Hanten <richard.hanten@uni-tuebingen.de>
2017-02-22 16:48:42 +0100

    region growing did not work and was slow as hell

--- csapex_point_cloud/src/segmentation/cluster_pcl.cpp
+++ csapex_point_cloud/src/segmentation/cluster_pcl.cpp
@@ -57,8 +57,6 @@ void ClusterPointcloud::setupParameters(Parameterizable &parameters)
 
     std::map<std::string, int> methods = {
         {"PCL_EUCLIDEAN", (int) Method::PCL_EUCLIDEAN},
-        {"PCL_REGION_GROWING", (int) Method::PCL_REGION_GROWING},
-        {"PCL_NORMAL", (int) Method::PCL_NORMAL},
         {"PCL_POLAR", (int) Method::PCL_POLAR},
     };
     parameters.addParameter(param::ParameterFactory::declareParameterSet("method", methods, (int) Method::PCL_EUCLIDEAN),
@@ -67,31 +65,6 @@ void ClusterPointcloud::setupParameters(Parameterizable &parameters)
     parameters.addParameter(param::ParameterFactory::declareRange("cluster tolerance", 0.001, 2.0, 0.02, 0.001), cluster_tolerance_);
     parameters.addParameter(param::ParameterFactory::declareRange("minimum cluster size", 0, 20000, 100, 1),          cluster_min_size_);
     parameters.addParameter(param::ParameterFactory::declareRange("maximum cluster size", 0, 100000, 25000, 1),       cluster_max_size_);
-
-    parameters.addConditionalParameter(param::ParameterFactory::declareRange("normal est. k nearest", 0, 500, 50, 1),
-                                       [this]{return method_ == Method::PCL_NORMAL || method_ == Method::PCL_REGION_GROWING;},
-    normal_estimator_k_nearest_);
-    parameters.addConditionalParameter(param::ParameterFactory::declareRange("normal est. radius", 0.0, 2.0, 0.02, 0.001),
-                                       [this]{return method_ == Method::PCL_REGION_GROWING;},
-    normal_estimator_radius_);
-
-    parameters.addConditionalParameter(param::ParameterFactory::declareRange("region growing neighbours", 1, 500, 50, 1),
-                                       [this]{return method_ == Method::PCL_REGION_GROWING;},
-    region_growing_neighbours_);
-
-    parameters.addConditionalParameter(param::ParameterFactory::declareAngle("region growing smoothness", 3.0 / 180.0 * M_PI),
-                                       [this]{return method_ == Method::PCL_REGION_GROWING;},
-    region_growing_smoothness_);
-    parameters.addConditionalParameter(param::ParameterFactory::declareRange("region growing threshold", 0.0, 5.0, 1.0, 0.001),
-                                       [this]{return method_ == Method::PCL_REGION_GROWING;},
-    region_growing_curvature_threshold_);
-
-    parameters.addConditionalParameter(param::ParameterFactory::declareRange("normal small radius", 0.001, 2.0, 0.02, 0.001),
-                                       [this]{return method_ == Method::PCL_NORMAL;},
-    normal_small_radius_);
-    parameters.addConditionalParameter(param::ParameterFactory::declareRange("normal large radius", 0.001, 2.0, 0.02, 0.001),
-                                       [this]{return method_ == Method::PCL_NORMAL;},
-    normal_large_radius_);
 
     parameters.addConditionalParameter(param::ParameterFactory::declareAngle("opening_angle", 0.001),
                                        [this](){return method_ == Method::PCL_POLAR;} ,
@@ -160,11 +133,6 @@ void ClusterPointcloud::inputCloud(typename pcl::PointCloud<PointT>::ConstPtr cl
     case Method::PCL_EUCLIDEAN:
         cluster_indices = pclEuclidean<PointT>(cloud, indices);
         break;
-    case Method::PCL_REGION_GROWING:
-        cluster_indices = pclRegionGrowing<PointT>(cloud, indices);
-        break;
-    case Method::PCL_NORMAL:
-        break;
     case Method::PCL_POLAR:
         cluster_indices = pclPolar<PointT>(cloud, indices);
         break;
@@ -214,49 +182,6 @@ ClusterPointcloud::pclEuclidean(typename pcl::PointCloud<PointT>::ConstPtr cloud
     return cluster_indices;
 }
 
-template <class PointT>
-std::shared_ptr<std::vector<pcl::PointIndices> >
-ClusterPointcloud::pclRegionGrowing(typename pcl::PointCloud<PointT>::ConstPtr cloud, pcl::IndicesConstPtr indices)
-{
-    typename pcl::search::KdTree<PointT>::Ptr tree (new pcl::search::KdTree<PointT>);
-    tree->setInputCloud(cloud, indices);
-
-    pcl::PointCloud <pcl::Normal>::Ptr                normals (new pcl::PointCloud <pcl::Normal>);
-    typename pcl::NormalEstimationOMP<PointT, pcl::Normal> normal_estimator;
-    {
-        INTERLUDE("normal estimation");
-        normal_estimator.setSearchMethod(tree);
-        normal_estimator.setIndices(indices);
-        normal_estimator.setInputCloud(cloud);
-        normal_estimator.setKSearch(normal_estimator_k_nearest_);
-        normal_estimator.setRadiusSearch(normal_estimator_radius_);
-        normal_estimator.compute (*normals);
-    }
-    std::shared_ptr<std::vector<pcl::PointIndices> > cluster_indices(new std::vector<pcl::PointIndices>);
-    {
-        INTERLUDE("clustering");
-        typename pcl::RegionGrowing<PointT, pcl::Normal> reg;
-        reg.setMinClusterSize(cluster_min_size_);
-        reg.setMaxClusterSize(cluster_max_size_);
-        reg.setSearchMethod(tree);
-        reg.setNumberOfNeighbours(region_growing_neighbours_);
-        reg.setIndices(indices);
-        reg.setInputCloud(cloud);
-        reg.setInputNormals(normals);
-        reg.setSmoothnessThreshold(region_growing_smoothness_);
-        reg.setCurvatureThreshold(region_growing_curvature_threshold_);
-        reg.extract(*cluster_indices);
-    }
-    return cluster_indices;
-}
-
-template <class PointT>
-std::shared_ptr<std::vector<pcl::PointIndices> >
-ClusterPointcloud::pclNormal(typename pcl::PointCloud<PointT>::ConstPtr cloud, pcl::IndicesConstPtr indices)
-{
-
-
-}
 
 template <class PointT>
 std::shared_ptr<std::vector<pcl::PointIndices> >
--- csapex_point_cloud/src/segmentation/cluster_pcl.h
+++ csapex_point_cloud/src/segmentation/cluster_pcl.h
@@ -30,24 +30,12 @@ private:
 
     template <class PointT>
     std::shared_ptr<std::vector<pcl::PointIndices> >
-    pclRegionGrowing(typename pcl::PointCloud<PointT>::ConstPtr cloud,
-                     pcl::IndicesConstPtr indices);
-
-    template <class PointT>
-    std::shared_ptr<std::vector<pcl::PointIndices> >
-    pclNormal(typename pcl::PointCloud<PointT>::ConstPtr cloud,
-              pcl::IndicesConstPtr indices);
-
-    template <class PointT>
-    std::shared_ptr<std::vector<pcl::PointIndices> >
     pclPolar(typename pcl::PointCloud<PointT>::ConstPtr cloud,
              pcl::IndicesConstPtr indices);
 
 private:
     enum class Method {
         PCL_EUCLIDEAN,
-        PCL_REGION_GROWING,
-        PCL_NORMAL,
         PCL_POLAR
     };
 
@@ -63,19 +51,7 @@ private:
     int     cluster_min_size_;
     int     cluster_max_size_;
 
-    int     normal_estimator_k_nearest_;
-    double  normal_estimator_radius_;
-
-    int     region_growing_neighbours_;
-    double  region_growing_smoothness_;
-    double  region_growing_curvature_threshold_;
-
-    double  normal_large_radius_;
-    double  normal_small_radius_;
-
-
     double  polar_opening_angle_;
-
 
 };
 }
