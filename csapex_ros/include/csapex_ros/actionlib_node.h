384e8eee0317f86ac2a78d9be33045c92bb5b961
buck <sebastian.buck@uni-tuebingen.de>
2017-03-01 02:34:12 +0100

    action lib node fixes

--- csapex_ros/include/csapex_ros/actionlib_node.h
+++ csapex_ros/include/csapex_ros/actionlib_node.h
@@ -4,6 +4,7 @@
 /// COMPONENT
 #include <csapex_ros/ros_node.h>
 #include <csapex/msg/io.h>
+#include <csapex/profiling/interlude.hpp>
 
 /// SYSTEM
 #include <actionlib/client/simple_action_client.h>
@@ -48,10 +49,12 @@ class ActionlibNode : public ActionlibNodeBase
 protected:
     virtual void abortAction() override
     {
+        INTERLUDE("abort");
+
         if(active_client_) {
             active_client_->cancelAllGoals();
 
-            active_client_.reset();
+//            active_client_.reset();
         }
 
         if(continuation_) {
@@ -62,9 +65,11 @@ protected:
 
     virtual void detachAction() override
     {
+        INTERLUDE("detach");
+
         if(active_client_) {
             active_client_->stopTrackingGoal();
-            active_client_.reset();
+//            active_client_.reset();
         }
 
         if(continuation_) {
@@ -77,6 +82,8 @@ protected:
 
     virtual void resultCallback(const actionlib::SimpleClientGoalState& state, const ResultConstPtr& result)
     {
+        INTERLUDE("result callback");
+
         if(!result) {
             aerr << "Received an empty result message. Did the action server crash?" << std::endl;
             msg::trigger(event_error_);
@@ -85,7 +92,7 @@ protected:
             processResultCallback(state, result);
         }
 
-        active_client_.reset();
+//        active_client_.reset();
         if(continuation_) {
             continuation_([](csapex::NodeModifier& node_modifier, Parameterizable &parameters){});
             continuation_ = Continuation();
@@ -113,6 +120,7 @@ protected:
 
     void sendGoal(const Goal& goal)
     {
+        INTERLUDE("send goal");
         startTimer();
         active_client_->sendGoal(goal,
                          boost::bind(&ActionlibNode<ActionSpec>::resultCallback, this, _1, _2),
@@ -124,7 +132,9 @@ protected:
                          ros::Duration execution_timeout = ros::Duration(0,0),
                          ros::Duration preempt_timeout = ros::Duration(0,0))
     {
+        INTERLUDE("send goal & wait");
         active_client_->sendGoalAndWait(goal, execution_timeout, preempt_timeout);
+//        active_client_.reset();
     }
 
     void setupClient(const std::string& topic, bool spin_thread)
@@ -142,27 +152,30 @@ class ChanneledActionlibNode : public ActionlibNode<ActionSpec>
 {
     ACTION_DEFINITION(ActionSpec);
 
-    using ActionlibNode<ActionSpec>::sendGoal;
-    using ActionlibNode<ActionSpec>::active_client_;
-    using ActionlibNode<ActionSpec>::ainfo;
-    using ActionlibNode<ActionSpec>::awarn;
-    using ActionlibNode<ActionSpec>::aerr;
+    using Parent = ActionlibNode<ActionSpec>;
+    using Parent::sendGoal;
+    using Parent::active_client_;
+    using Parent::ainfo;
+    using Parent::awarn;
+    using Parent::aerr;
+    using Parent::event_error_;
 
 protected:
     void processROS()
     {
         std::string channel = getChannel();
-
-        auto pos = clients_.find(channel);
-        if(pos == clients_.end()) {
-            clients_[channel] = std::make_shared<actionlib::SimpleActionClient<ActionSpec>>(channel, true);
+        if(active_client_) {
+            active_client_->cancelAllGoals();
+            active_client_.reset();
         }
-        active_client_ = clients_.at(channel);
+        active_client_ = std::make_shared<actionlib::SimpleActionClient<ActionSpec>>(channel, true);
 
         if(!active_client_->isServerConnected()) {
             awarn << "waiting for action server channel " << channel << std::endl;
             if(!active_client_->waitForServer(ros::Duration(1.0))) {
-                throw std::runtime_error(std::string("unknown action server channel ") + channel);
+                aerr << "unknown action server channel " + channel << std::endl;;
+                msg::trigger(event_error_);
+                return;
             }
         }
 
@@ -175,7 +188,7 @@ protected:
     virtual void getGoal(Goal& goal) = 0;
 
 protected:
-    std::map<std::string, std::shared_ptr<actionlib::SimpleActionClient<ActionSpec>>> clients_;
+//    std::map<std::string, std::shared_ptr<actionlib::SimpleActionClient<ActionSpec>>> clients_;
 };
 
 }
