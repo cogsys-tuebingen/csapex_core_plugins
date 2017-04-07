b13229ef327b12c7c1b19c892ea2c8e3b3146457
buck <sebastian.buck@uni-tuebingen.de>
2017-04-07 12:29:29 +0200

    minor

---
+++ csapex_core_plugins/src/tools/overwrite_timestamp.cpp
@@ -0,0 +1,59 @@
+
+/// PROJECT
+#include <csapex/model/node.h>
+#include <csapex/msg/io.h>
+#include <csapex/param/parameter_factory.h>
+#include <csapex/model/node_modifier.h>
+#include <csapex/utility/register_apex_plugin.h>
+#include <csapex/msg/any_message.h>
+#include <csapex_core_plugins/timestamp_message.h>
+
+using namespace csapex;
+using namespace csapex::connection_types;
+
+namespace csapex
+{
+
+
+class OverwriteTimestamp : public Node
+{
+public:
+    OverwriteTimestamp()
+    {
+    }
+
+    void setup(csapex::NodeModifier& modifier) override
+    {
+        in_ = modifier.addInput<AnyMessage>("Input");
+        in_time_ = modifier.addInput<TimestampMessage>("Time");
+        out_ = modifier.addOutput<AnyMessage>("Output");
+    }
+
+    void setupParameters(csapex::Parameterizable& params) override
+    {
+    }
+
+    void process() override
+    {
+        TokenDataConstPtr message = msg::getMessage(in_);
+        MessagePtr message_clone = message->cloneAs<Message>();
+        if(message_clone) {
+            TimestampMessage::ConstPtr time = msg::getMessage<TimestampMessage>(in_time_);
+
+            message_clone->stamp_micro_seconds = time->stamp_micro_seconds;
+            msg::publish(out_, message_clone);
+        }
+    }
+
+private:
+    Input* in_;
+    Input* in_time_;
+    Output* out_;
+
+};
+
+} // csapex
+
+
+CSAPEX_REGISTER_CLASS(csapex::OverwriteTimestamp, csapex::Node)
+
