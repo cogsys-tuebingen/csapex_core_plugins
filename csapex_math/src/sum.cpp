bc037fa17672c15289f38eb0e940637d23ba51f7
buck <sebastian.buck@uni-tuebingen.de>
2018-02-18 04:02:07 +0100

    Made Sum variadic and allow non-vector input

--- csapex_math/src/sum.cpp
+++ csapex_math/src/sum.cpp
@@ -9,6 +9,7 @@
 #include <csapex/model/token.h>
 #include <csapex/msg/generic_vector_message.hpp>
 #include <csapex/msg/generic_value_message.hpp>
+#include <csapex/model/variadic_io.h>
 
 /// SYSTEM
 #include <algorithm>
@@ -21,7 +22,7 @@ namespace csapex
 {
 
 
-class Sum : public Node
+class Sum : public Node, public VariadicInputs
 {
 public:
     Sum()
@@ -30,41 +31,92 @@ public:
 
     void setup(csapex::NodeModifier& modifier) override
     {
-        in_ = modifier.addInput<GenericVectorMessage>("Input");
-        out_= modifier.addOutput<double>("Reduced");
+        setupVariadic(modifier);
+
+        out_= modifier.addOutput<double>("Sum");
     }
 
     void setupParameters(csapex::Parameterizable& params) override
     {
+        setupVariadicParameters(params);
+    }
+
+    virtual Input* createVariadicInput(csapex::TokenDataConstPtr type, const std::string& label, bool optional) override
+    {
+        return VariadicInputs::createVariadicInput(multi_type::make<
+                                                       GenericVectorMessage,
+                                                       GenericValueMessage<int>,
+                                                       GenericValueMessage<double>
+                                                   >(),
+                                                   label.empty() ? "Vector<Number> or Number" : label,
+                                                   getVariadicInputCount() == 0 ? false : true);
     }
 
     void process() override
     {
-        GenericVectorMessage::ConstPtr vec = msg::getMessage<GenericVectorMessage>(in_);
-        if(std::dynamic_pointer_cast<GenericValueMessage<double>>(vec->nestedType())) {
-            acc<double>(vec);
-        } else if(std::dynamic_pointer_cast<GenericValueMessage<int>>(vec->nestedType())) {
-            acc<int>(vec);
-        }
-    }
+        std::size_t num_inputs = VariadicInputs::getVariadicInputCount();
 
-    template <typename T>
-    void acc(GenericVectorMessage::ConstPtr vec )
-    {
-        double sum = 0;
-        for(std::size_t i = 0, n = vec->nestedValueCount(); i < n; ++i) {
-            if(auto pval = std::dynamic_pointer_cast<GenericValueMessage<T> const>(vec->nestedValue(i))) {
-                sum += pval->value;
+        sum = 0;
+
+        for(std::size_t i_inputs = 0; i_inputs < num_inputs; ++i_inputs){
+            addInput(VariadicInputs::getVariadicInput(i_inputs));
+        }
+
+        InputPtr in = VariadicInputs::getVariadicInput(0);
+        if(msg::isValue<int>(in.get())) {
+            int out = (int) sum;
+            msg::publish(out_, out);
+
+        } else if(msg::isValue<double>(in.get())) {
+            msg::publish(out_, sum);
+
+        } else {
+            // must be vector
+            GenericVectorMessageConstPtr vec = msg::getMessage<GenericVectorMessage>(in.get());
+            if(std::dynamic_pointer_cast<connection_types::GenericValueMessage<int> const>(vec->nestedType())) {
+                int out = (int) sum;
+                msg::publish(out_, out);
+            } else {
+                msg::publish(out_, sum);
             }
         }
 
         msg::publish(out_, sum);
     }
 
+    void addInput(const InputPtr& in)
+    {
+        if(msg::isValue<int>(in.get())) {
+            sum += msg::getValue<int>(in.get());
+
+        } else if(msg::isValue<double>(in.get())) {
+            sum += msg::getValue<double>(in.get());
+
+        } else {
+            // must be vector
+            GenericVectorMessageConstPtr vec = msg::getMessage<GenericVectorMessage>(in.get());
+            if(std::dynamic_pointer_cast<GenericValueMessage<double>>(vec->nestedType())) {
+                acc<double>(vec);
+            } else if(std::dynamic_pointer_cast<GenericValueMessage<int>>(vec->nestedType())) {
+                acc<int>(vec);
+            }
+        }
+    }
+
+    template <typename T>
+    void acc(GenericVectorMessage::ConstPtr vec )
+    {
+        for(std::size_t i = 0, n = vec->nestedValueCount(); i < n; ++i) {
+            if(auto pval = std::dynamic_pointer_cast<GenericValueMessage<T> const>(vec->nestedValue(i))) {
+                sum += pval->value;
+            }
+        }
+    }
+
 private:
-    Input* in_;
     Output* out_;
 
+    double sum;
 };
 
 } // csapex
