1c6d0160b82455cac0f85067256910267f876144
buck <sebastian.buck@uni-tuebingen.de>
2018-03-20 22:07:55 +0100

    Demonstrate message versioning

--- csapex_tutorial/src/tutorial_message.cpp
+++ csapex_tutorial/src/tutorial_message.cpp
@@ -19,8 +19,9 @@ TutorialMessage::TutorialMessage()
 namespace YAML {
 Node convert<csapex::connection_types::TutorialMessage>::encode(const csapex::connection_types::TutorialMessage& rhs)
 {
-    Node node = convert<csapex::connection_types::Message>::encode(rhs);
-    node["value"] = rhs.value;
+    Node node = convert<csapex::connection_types::Message>::encode(rhs, {2, 0, 0});
+    node["value2"] = rhs.value;
+
     return node;
 }
 
@@ -29,8 +30,13 @@ bool convert<csapex::connection_types::TutorialMessage>::decode(const Node& node
     if(!node.IsMap()) {
         return false;
     }
-    convert<csapex::connection_types::Message>::decode(node, rhs);
-    rhs.value = node["value"].as<bool>();
+    Message::Version version = convert<csapex::connection_types::Message>::decode(node, rhs);
+    if(version < Message::Version{2, 0, 0}) {
+        rhs.value = node["value"].as<bool>();
+    } else {
+        rhs.value = node["value2"].as<bool>();
+    }
+
     return true;
 }
 }
