3724aa4c696e5b026cf14d5972ff53f890db2146
Richard Hanten <richard.hanten@uni-tuebingen.de>
2018-08-13 17:06:10 +0200

    illumination

--- csapex_vision/src/color/bgr_to_illumination_invariant.cpp
+++ csapex_vision/src/color/bgr_to_illumination_invariant.cpp
@@ -13,59 +13,94 @@ using namespace connection_types;
 class BGRToIlluminationInvariant : public csapex::Node
 {
 public:
-    BGRToIlluminationInvariant() = default;
-    virtual ~BGRToIlluminationInvariant() = default;
+  BGRToIlluminationInvariant() = default;
+  virtual ~BGRToIlluminationInvariant() = default;
 
-    virtual void process() override
-    {
-        CvMatMessage::ConstPtr in = msg::getMessage<connection_types::CvMatMessage>(input_);
-        if(!in->getEncoding().matches(enc::bgr))
-          throw std::runtime_error("Image needs to be BGR for fitting conversion.");
+  virtual void process() override
+  {
+    CvMatMessage::ConstPtr in = msg::getMessage<connection_types::CvMatMessage>(input_);
+    if(!in->getEncoding().matches(enc::bgr))
+      throw std::runtime_error("Image needs to be BGR for fitting conversion.");
 
-        CvMatMessage::Ptr out(new connection_types::CvMatMessage(enc::unknown, in->frame_id, in->stamp_micro_seconds));
-        out->value = cv::Mat(in->value.rows, in->value.cols, CV_32FC1, cv::Scalar());
-        CvMatMessage::Ptr out_mask(new connection_types::CvMatMessage(enc::mono, in->frame_id, in->stamp_micro_seconds));
-        out_mask->value = cv::Mat(in->value.rows, in->value.cols, CV_8UC1, cv::Scalar(255));
+    CvMatMessage::Ptr out(new connection_types::CvMatMessage(enc::unknown, in->frame_id, in->stamp_micro_seconds));
+    out->value = cv::Mat(in->value.rows, in->value.cols, CV_32FC1, cv::Scalar());
+    CvMatMessage::Ptr out_mask(new connection_types::CvMatMessage(enc::mono, in->frame_id, in->stamp_micro_seconds));
+    out_mask->value = cv::Mat(in->value.rows, in->value.cols, CV_8UC1, cv::Scalar(255));
 
-
-        const cv::Vec3b *src = in->value.ptr<cv::Vec3b>();
-        float *dst  = out->value.ptr<float>();
-        uchar *mask = out_mask->value.ptr<uchar>();
-        const std::size_t size = static_cast<std::size_t>(out->value.cols * out->value.rows);
-        for(std::size_t i = 0 ; i < size ; ++i) {
-            const cv::Vec3b &p = src[i];
-            if(p[0] == 0 || p[1] == 0 || p[2] == 0) {
-              mask[i] = 0;
-            }
-            dst[i] = 0.5 + std::log(p[1]) - alpha_ * std::log(p[0]) - (1.0 - alpha_) * std::log(p[2]);
+    if(method_ == Newman) {
+      const cv::Vec3b *src = in->value.ptr<cv::Vec3b>();
+      float *dst  = out->value.ptr<float>();
+      uchar *mask = out_mask->value.ptr<uchar>();
+      const std::size_t size = static_cast<std::size_t>(out->value.cols * out->value.rows);
+      for(std::size_t i = 0 ; i < size ; ++i) {
+        const cv::Vec3b &p = src[i];
+        if(p[0] == 0 || p[1] == 0 || p[2] == 0) {
+          mask[i] = 0;
         }
+        cv::Vec3f c(p[0] / 255.0, p[1] / 255.0, p[2] / 255.0);
+        dst[i] = 0.5 + std::log(c[1]) - alpha_ * std::log(c[0]) - (1.0 - alpha_) * std::log(c[2]);
+      }
+    }
+    if(method_ == Santos) {
+      const cv::Vec3b *src = in->value.ptr<cv::Vec3b>();
+      float *dst  = out->value.ptr<float>();
+      uchar *mask = out_mask->value.ptr<uchar>();
+      const std::size_t size = static_cast<std::size_t>(out->value.cols * out->value.rows);
 
-        msg::publish(output_, out);
-        msg::publish(output_mask_, out_mask);
+      const static cv::Vec3f v1(1.0 / std::sqrt(2.0), -1.0 / std::sqrt(2.0), 0.0);
+      const static cv::Vec3f v2(1.0 / std::sqrt(6.0),  1.0 / std::sqrt(6.0), -2.0 / std::sqrt(6.0));  //// Entropy Minimization for Shadow Removal
+      const double theta = M_PI * alpha_;
+
+      for(std::size_t i = 0 ; i < size ; ++i) {
+        const cv::Vec3b &p = src[i];
+        if(p[0] == 0 || p[1] == 0 || p[2] == 0) {
+          mask[i] = 0;
+        }
+        cv::Vec3f c(p[0] / 255.0, p[1] / 255.0, p[2] / 255.0);
+        const double rho = std::sqrt(c.ddot(c));
+        c /= rho;
+        const cv::Vec2f psi(c.ddot(v1), c.ddot(v2));
+
+
+        dst[i] = psi[0] * std::cos(theta) + psi[1]  * std::sin(theta);
+      }
     }
 
-    virtual void setup(csapex::NodeModifier& node_modifier) override
-    {
-        input_ = node_modifier.addInput<CvMatMessage>("original");
-        output_ = node_modifier.addOutput<CvMatMessage>("adjusted");
-        output_mask_ = node_modifier.addOutput<CvMatMessage>("mask");
-    }
+    msg::publish(output_, out);
+    msg::publish(output_mask_, out_mask);
+  }
 
-    virtual void setupParameters(Parameterizable &parameters)
-    {
-        parameters.addParameter(param::ParameterFactory::declareRange("alpha", 0.0, 1.0, 1.5, 0.001),
-                                alpha_);
-        parameters.addParameter(param::ParameterFactory::declareBool("normalize", true),
-                                normalize_);
-    }
+  virtual void setup(csapex::NodeModifier& node_modifier) override
+  {
+    input_ = node_modifier.addInput<CvMatMessage>("original");
+    output_ = node_modifier.addOutput<CvMatMessage>("adjusted");
+    output_mask_ = node_modifier.addOutput<CvMatMessage>("mask");
+  }
+
+  virtual void setupParameters(Parameterizable &parameters)
+  {
+    parameters.addParameter(param::ParameterFactory::declareRange("alpha", 0.0, 1.0, 1.5, 0.001),
+                            alpha_);
+    std::map<std::string, int> methods = {
+      {"Newman", (int) Newman},
+      {"Santos", (int) Santos},
+    };
+
+    parameters.addParameter(param::ParameterFactory::declareParameterSet<int>("method", methods, Newman),
+                            method_);
+
+  }
 
 protected:
-    csapex::Output*   output_;
-    csapex::Output*   output_mask_;
+  csapex::Output*   output_;
+  csapex::Output*   output_mask_;
 
-    csapex::Input*    input_;
-    double alpha_;
-    bool normalize_;
+  enum Type {Newman, Santos};
+
+  csapex::Input*    input_;
+  double            alpha_;
+  int               method_;
+  bool              recover_bgr_;
 };
 }
 
