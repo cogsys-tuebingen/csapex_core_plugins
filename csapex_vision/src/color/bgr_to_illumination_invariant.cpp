603fb07011832ab5bb3c6e64ecb9d9edd8fcde0d
Christian Geckeler <christian-marius.geckeler@student.uni-tuebingen.de>
2018-08-20 13:57:25 +0200

    illumination invariance normalize min/max

--- csapex_vision/src/color/bgr_to_illumination_invariant.cpp
+++ csapex_vision/src/color/bgr_to_illumination_invariant.cpp
@@ -28,29 +28,48 @@ public:
         out_mask->value = cv::Mat(in->value.rows, in->value.cols, CV_8UC1, cv::Scalar(255));
 
         switch(method_) {
+        case NewmanOriginal:
+        {
+          const cv::Vec3b *src = in->value.ptr<cv::Vec3b>();
+          float *dst  = out->value.ptr<float>();
+          uchar *mask = out_mask->value.ptr<uchar>();
+          const std::size_t size = static_cast<std::size_t>(out->value.cols * out->value.rows);
+
+          for(std::size_t i = 0 ; i < size ; ++i) {
+              const cv::Vec3b &p = src[i];
+              if(p[0] == 0 || p[1] == 0 || p[2] == 0) {
+                  mask[i] = 0;
+                  continue;
+              }
+              cv::Vec3f c(p[0] / 255.0, p[1] / 255.0, p[2] / 255.0);
+              dst[i] = 0.5 + std::log(c[1]) - alpha_ * std::log(c[0]) - (1.0 - alpha_) * std::log(c[2]);
+          }
+        }
+            break;
         case Newman:
         {
+            double min = 0.5 - std::log(255.0);
+            double max = 0.5 + std::log(255.0);
+
             const cv::Vec3b *src = in->value.ptr<cv::Vec3b>();
             float *dst  = out->value.ptr<float>();
             uchar *mask = out_mask->value.ptr<uchar>();
             const std::size_t size = static_cast<std::size_t>(out->value.cols * out->value.rows);
-            double min = 2 * std::log(1.0 / 255.0);
 
             for(std::size_t i = 0 ; i < size ; ++i) {
                 const cv::Vec3b &p = src[i];
+                cv::Vec3f c(p);
                 if(p[0] == 0 || p[1] == 0 || p[2] == 0) {
                     mask[i] = 0;
-
+                    continue;
                 }
-                cv::Vec3f c(p[0] / 255.0, p[1] / 255.0, p[2] / 255.0);
-                dst[i] = (0.5 + std::log(c[1]) - alpha_ * std::log(c[0]) - (1.0 - alpha_) * std::log(c[2]) + min) / (-min + 0.5);
+                dst[i] = (0.5 + std::log(c[1]) - alpha_ * std::log(c[0]) - (1.0 - alpha_) * std::log(c[2]));
+                dst[i] = ((dst[i] - min) / (max - min)); //normalize 0-1
+//                dst[i] = dst[i] * 255.0; //discretize 0-255
             }
-
-            /// min = 2 * std::log(1. / 255.)
-            /// max = 2 * std::log(1.0)
         }
             break;
-        case Santos:
+        case SantosOriginal:
         {
             const cv::Vec3b *src = in->value.ptr<cv::Vec3b>();
             float *dst  = out->value.ptr<float>();
@@ -86,7 +105,7 @@ public:
         }
             break;
 
-        case SantosOriginal:
+        case Santos:
         {
             const cv::Vec3b *src = in->value.ptr<cv::Vec3b>();
             float *dst  = out->value.ptr<float>();
@@ -99,7 +118,9 @@ public:
             const static cv::Vec3f v1(1.0 / std::sqrt(2.0), -1.0 / std::sqrt(2.0), 0.0);
             const static cv::Vec3f v2(1.0 / std::sqrt(6.0),  1.0 / std::sqrt(6.0), -2.0 / std::sqrt(6.0));  //// Entropy Minimization for Shadow Removal
 
-            double max = std::log(1.0 / std::pow(((1.0 / 255.0) * (1.0 / 255.0) * (1.0 / 255.0)), 1.0 / 3.0)) / std::sqrt(2.0);
+            double max = std::log(255.0) * (0.5 + (1 / std::sqrt(3)));
+            double min = - max;
+
             for(std::size_t i = 0 ; i < size ; ++i) {
                 const cv::Vec3b &p = src[i];
                 if(p[0] == 0 || p[1] == 0 || p[2] == 0) {
@@ -107,14 +128,18 @@ public:
                     dst[i] = 0;
                     continue;
                 }
-                cv::Vec3f c(p[2] / 255.0, p[1] / 255.0, p[1] / 255.0);
+
+                cv::Vec3f c(p[0] / 255.0, p[1] / 255.0, p[2] / 255.0);
+
                 const double rho = std::pow((c[0] * c[1] * c[2]), 1.0 / 3);
                 c /= rho;
+                c[0] = std::log(c[0]);
+                c[1] = std::log(c[1]);
+                c[2] = std::log(c[2]);
                 const cv::Vec2f psi(c.ddot(v1), c.ddot(v2));
-                dst[i] = (psi[0] * ct + psi[1]  * st) / max;
+                dst[i] = ((psi[0] * ct + psi[1]  * st) - min) / (max - min); //normalize
+//                dst[i] = dst[i] * 255.0; //discretize 0-255
             }
-            /// sqrt3(1) <=> sqrt3((1/255)^3) < 1.0
-            /// ct + st <= sqrt(2)
         }
             break;
         default:
@@ -137,6 +162,7 @@ public:
                                 alpha_);
         std::map<std::string, int> methods = {
             {"Newman", (int) Newman},
+            {"NewmanOriginal", (int) NewmanOriginal},
             {"Santos", (int) Santos},
             {"SantosOriginal", (int) SantosOriginal}
         };
@@ -150,7 +176,7 @@ protected:
     csapex::Output*   output_;
     csapex::Output*   output_mask_;
 
-    enum Type {Newman, Santos, SantosOriginal};
+    enum Type {Newman, NewmanOriginal, Santos, SantosOriginal};
 
     csapex::Input*    input_;
     double            alpha_;
