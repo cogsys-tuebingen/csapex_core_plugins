413f88add6a10d8a32f697384c1ff8e3b15ef889
Adrian Zwiener <adrian.zwiener@uni-tuebingen.de>
2018-09-08 20:12:17 +0200

    Add a node and add multi input to RadiusOutlierFiler

---
+++ csapex_point_cloud/src/conversion/vector_of_indicies_to_indices.cpp
@@ -0,0 +1,63 @@
+
+/// PROJECT
+#include <csapex/msg/io.h>
+#include <csapex/model/node_modifier.h>
+#include <csapex/msg/generic_vector_message.hpp>
+#include <csapex/param/parameter_factory.h>
+#include <csapex/utility/register_apex_plugin.h>
+#include <csapex_point_cloud/msg/indices_message.h>
+#include <csapex/model/node.h>
+#include <csapex/msg/generic_vector_message.hpp>
+#include <csapex_point_cloud/msg/point_cloud_message.h>
+#include <csapex_opencv/cv_mat_message.h>
+
+
+using namespace csapex;
+using namespace csapex::connection_types;
+
+namespace csapex
+{
+
+
+class VectorOfIndiciesToIndices : public Node
+{
+public:
+    VectorOfIndiciesToIndices()
+    {
+    }
+
+    void setup(csapex::NodeModifier& modifier) override
+    {
+        in_ = modifier.addInput<GenericVectorMessage, pcl::PointIndices>("PointIndices");
+        out_= modifier.addOutput<PointIndicesMessage>("PointIndices");
+    }
+
+    void setupParameters(csapex::Parameterizable& params) override
+    {
+    }
+
+    void process() override
+    {
+        auto vector = msg::getMessage<GenericVectorMessage, pcl::PointIndices>(in_);
+
+        PointIndicesMessage::Ptr out(new PointIndicesMessage);
+        if(!vector->empty()){
+            out->value->header = vector->front().header;
+            for(auto indices : *vector){
+                out->value->indices.insert(out->value->indices.end(), indices.indices.begin(), indices.indices.end());
+            }
+        }
+        msg::publish(out_, out);
+    }
+
+private:
+    Input* in_;
+    Output* out_;
+
+};
+
+} // csapex
+
+
+CSAPEX_REGISTER_CLASS(csapex::VectorOfIndiciesToIndices, csapex::Node)
+
--- csapex_point_cloud/src/filter/radius_outlier_removal.cpp
+++ csapex_point_cloud/src/filter/radius_outlier_removal.cpp
@@ -11,9 +11,11 @@
 /// SYSTEM
 
 #include <csapex/utility/suppress_warnings_start.h>
-    #include <pcl/point_types.h>
-    #include <pcl/filters/radius_outlier_removal.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/radius_outlier_removal.h>
 #include <csapex/utility/suppress_warnings_end.h>
+#include <csapex/msg/generic_vector_message.hpp>
+#include <csapex/msg/any_message.h>
 
 CSAPEX_REGISTER_CLASS(csapex::RadiusOutlierRemoval, csapex::Node)
 
@@ -29,15 +31,15 @@ void RadiusOutlierRemoval::setupParameters(Parameterizable &parameters)
     parameters.addParameter(csapex::param::factory::declareRange("min neighbours", 1, 1000, 2, 1));
     parameters.addParameter(csapex::param::factory::declareBool ("keep organized", false));
     parameters.addParameter(csapex::param::factory::declareBool ("negate", false));
-    parameters.addParameter(csapex::param::factory::declareRange("search radius", 0.0, 30.0, 0.8, 0.1));
+    parameters.addParameter(csapex::param::factory::declareRange("search radius", 0.0, 30.0, 0.8, 0.01));
 }
 
 void RadiusOutlierRemoval::setup(NodeModifier& node_modifier)
 {
     input_cloud_ = node_modifier.addInput<PointCloudMessage>("PointCloud");
-    indices_input_ = node_modifier.addOptionalInput<PointIndicesMessage>("indices");
+    indices_input_ = node_modifier.addOptionalMultiInput<PointIndicesMessage,GenericVectorMessage>("indices");
     output_cloud_ = node_modifier.addOutput<PointCloudMessage>("Pointcloud");
-    output_indices_ = node_modifier.addOutput<PointIndicesMessage>("indices");
+    output_indices_ = node_modifier.addOutput<AnyMessage>("indices");
 }
 
 void RadiusOutlierRemoval::process()
@@ -52,6 +54,7 @@ void RadiusOutlierRemoval::inputCloud(typename pcl::PointCloud<PointT>::ConstPtr
 
     bool indices_out = msg::isConnected(output_indices_);
     bool cloud_out   = msg::isConnected(output_cloud_);
+    bool has_index = msg::hasMessage(indices_input_);
 
     if(!indices_out && !cloud_out)
         return;
@@ -64,31 +67,68 @@ void RadiusOutlierRemoval::inputCloud(typename pcl::PointCloud<PointT>::ConstPtr
     typename pcl::PointCloud<PointT>::Ptr cloud_wo_nan(new pcl::PointCloud<PointT>);
     std::vector<int> nan_indices;
     pcl::removeNaNFromPointCloud<PointT>(*cloud, *cloud_wo_nan, nan_indices);
-
     pcl::RadiusOutlierRemoval<PointT> ror;
+//    ror.setInputCloud(cloud);
     ror.setInputCloud(cloud_wo_nan);
     ror.setKeepOrganized(keep_organized_);
     ror.setNegative(negative_);
     ror.setRadiusSearch(search_radius_);
     ror.setMinNeighborsInRadius (min_neighbours_);
-    if(msg::hasMessage(indices_input_)) {
-        PointIndicesMessage::ConstPtr indices(msg::getMessage<PointIndicesMessage>(indices_input_));
-        ror.setIndices(indices->value);
-        if(indices->value->indices.size() == 0)
-            std::cout << "got empty" << std::endl;
-    }
-    if(cloud_out) {
-        typename pcl::PointCloud<PointT>::Ptr cloud_filtered(new pcl::PointCloud<PointT>);
+
+    typename pcl::PointCloud<PointT>::Ptr cloud_filtered(new pcl::PointCloud<PointT>);
+
+    if(has_index){
+        if(msg::isMessage<PointIndicesMessage>(indices_input_)){
+            PointIndicesMessage::ConstPtr indices = msg::getMessage<PointIndicesMessage>(indices_input_);
+            if(indices->value->indices.size() == 0)
+                std::cout << "got empty" << std::endl;
+            ror.setIndices(indices->value);
+            if(cloud_out) {
+                ror.filter(*cloud_filtered);
+                PointCloudMessage::Ptr out(new PointCloudMessage(cloud->header.frame_id, cloud->header.stamp));
+                cloud_filtered->header = cloud->header;
+                out->value = cloud_filtered;
+                msg::publish(output_cloud_, out);
+            }
+            if(indices_out) {
+                PointIndicesMessage::Ptr indices_filtered(new PointIndicesMessage);
+                indices_filtered->value->header = cloud->header;
+                ror.filter(indices_filtered->value->indices);
+                msg::publish(output_indices_, indices_filtered);
+            }
+        } else {
+            GenericVectorMessage::ConstPtr message = msg::getMessage<GenericVectorMessage>(indices_input_);
+            apex_assert(std::dynamic_pointer_cast<GenericPointerMessage<pcl::PointIndices> const>(message->nestedType()));
+            std::shared_ptr<std::vector<pcl::PointIndices>> out_ind(new std::vector<pcl::PointIndices>(message->nestedValueCount()));
+            auto it = out_ind->begin();
+            for(std::size_t i = 0; i < message->nestedValueCount(); ++i){
+                auto val = std::dynamic_pointer_cast<GenericPointerMessage<pcl::PointIndices> const>(message->nestedValue(i));
+                pcl::PointIndices::Ptr id = boost::make_shared<pcl::PointIndices>(*val->value);
+                ror.setIndices(id);
+                if(cloud_out){
+                    ror.filter(*cloud_filtered);
+                }
+                if(indices_out){
+                    ror.filter(it->indices);
+                    ++it;
+                }
+            }
+            if(cloud_out) {
+                PointCloudMessage::Ptr out(new PointCloudMessage(cloud->header.frame_id, cloud->header.stamp));
+                cloud_filtered->header = cloud->header;
+                out->value = cloud_filtered;
+                msg::publish(output_cloud_, out);
+            }
+            if(indices_out) {
+                msg::publish<GenericVectorMessage, pcl::PointIndices>(output_indices_, out_ind);
+            }
+        }
+    } else {
         ror.filter(*cloud_filtered);
         PointCloudMessage::Ptr out(new PointCloudMessage(cloud->header.frame_id, cloud->header.stamp));
         cloud_filtered->header = cloud->header;
         out->value = cloud_filtered;
         msg::publish(output_cloud_, out);
     }
-    if(indices_out) {
-        PointIndicesMessage::Ptr indices_filtered(new PointIndicesMessage);
-        indices_filtered->value->header = cloud->header;
-        ror.filter(indices_filtered->value->indices);
-        msg::publish(output_indices_, indices_filtered);
-    }
+
 }
