e9ef7bdc0a95639e0d1d766b080e28b2a6421b70
Sebastian Buck <mail@betwo.eu>
2020-08-15 12:52:39 +0200

    Add backwards compatibility to experimental::any

--- csapex_math/src/param/angle_parameter.cpp
+++ csapex_math/src/param/angle_parameter.cpp
@@ -6,9 +6,7 @@
 #include <csapex/param/value_parameter.h>
 #include <csapex/serialization/io/std_io.h>
 #include <csapex/utility/yaml.h>
-
-/// SYSTEM
-#include <any>
+#include <csapex/utility/any.h>
 
 CSAPEX_REGISTER_PARAM(AngleParameter)
 
